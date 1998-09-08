64c2c11574cef443beef2854c2f87dcabb545384
Brian Costello <btx@uncensored.citadel.org>
1998-09-08 03:13:39 +0000

    The dynloader.c header

---
+++ citadel/dynloader.h
@@ -0,0 +1,20 @@
+struct DLModule_Info
+{
+   char module_name[30];
+   char module_author[30];
+   char module_author_email[30];
+   int major_version, minor_version;
+};
+
+typedef struct s_symtab
+{
+   char *fcn_name;
+   char *server_cmd;
+   char *info_msg;
+   char *module_path;
+   struct s_symtab *next;
+} symtab;
+
+void DLoader_Init(char *pathname, symtab **);
+int DLoader_Exec_Cmd(char *cmdbuf);
+void add_symbol(char *fcn_name, char *server_cmd, char *info_msg, symtab **);
