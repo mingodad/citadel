e65bd5b6aa21d2b79dcbd4bd1efffc334717d5f9
Nathan Bryant <loanshark@uncensored.citadel.org>
1998-10-13 04:26:28 +0000

    * serv_upgrade.sh: remove uncnsrd-dependent absolute path

--- citadel/serv_upgrade.sh
+++ citadel/serv_upgrade.sh
@@ -3,4 +3,4 @@
 # Please don't add this to the Makefile
 #
 cc -O2 -Wall -D_REENTRANT -fPIC -c serv_upgrade.c
-cc -shared -o /appl/develcit/citadel/modules/serv_upgrade.so serv_upgrade.o
+cc -shared -o modules/serv_upgrade.so serv_upgrade.o
