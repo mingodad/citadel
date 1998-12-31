637e53a93202dfc64e45aea19bb5fe5861e9df76
Art Cancro <ajc@citadel.org>
1998-12-31 01:53:08 +0000

    Fleshed out the mime parser a bit

--- webcit/mime_parser.c
+++ webcit/mime_parser.c
@@ -5,9 +5,6 @@
  * messages -- in the case of WebCit, a form containing uploaded files.
  */
 
-
-
-
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
@@ -20,7 +17,45 @@
 
 void mime_parser(char *content, int ContentLength, char *ContentType) {
 	char boundary[256];
+	int have_boundary = 0;
+	int a;
+	char *ptr;
+	int bytes_processed = 0;
 
+	fprintf(stderr, "MIME: ContentLength: %d, ContentType: %s\n",
+		ContentLength, ContentType);
 
+	/* Figure out what the boundary is */
+	strcpy(boundary, ContentType);
+	for (a=0; a<strlen(boundary); ++a) {
+		if (!strncasecmp(&boundary[a], "boundary=", 9)) {
+			strcpy(boundary, &boundary[a+10]);
+			have_boundary = 1;
+			}
+		if ((boundary[a]==13) || (boundary[a]==10)) {
+			boundary[a] = 0;
+			}
+		}
 
+	/* We can't process multipart messages without a boundary. */
+	if (have_boundary == 0) return;
+
+	ptr = content;
+
+	/* Seek to the beginning of the next boundary */
+	while ( (bytes_processed < ContentLength)
+	      && (strncasecmp(ptr, boundary, strlen(boundary))) ) {
+		++ptr;
+		++bytes_processed;
+
+		/* Seek to the end of the boundary string */
+		if (!strncasecmp(ptr, boundary, strlen(boundary))) {
+			fprintf(stderr, "MIME: founda bounda\n");
+			while ( (bytes_processed < ContentLength)
+	      		      && (strncasecmp(ptr, "\n", 1)) ) {
+				++ptr;
+				++bytes_processed;
+				}
+			}
+		}
 	}
