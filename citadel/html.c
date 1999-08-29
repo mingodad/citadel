6cf23863e0961b0bcdd2470099af523c9a1da484
Art Cancro <ajc@citadel.org>
1999-08-29 19:56:43 +0000

    * HTML updates

--- citadel/html.c
+++ citadel/html.c
@@ -28,6 +28,7 @@
 #include "tools.h"
 #include "room_ops.h"
 #include "html.h"
+ 
 
 /*
  * Convert HTML to plain text.
@@ -37,48 +38,34 @@ char *html_to_ascii(char *inputmsg, int screenwidth) {
 	char outbuf[256];
 	char tag[1024];
 	int done_reading = 0;
-	char *readptr, *outputbuf;
-	size_t input_length, this_read, output_length;
-	int i, ch, did_out, rb;
+	char *inptr;
+	int i, j, ch, did_out, rb;
 	int nest = 0;		/* Bracket nesting level */
 
-	input_length = strlen(inputmsg);
-	readptr = inputmsg;
-	output_length = strlen(inputmsg);
-	outputbuf = mallok(output_length);
-	if (outputbuf==NULL) return NULL;
+	inptr = inputmsg;
 	strcpy(inbuf, "");
 	strcpy(outbuf, "");
-
-	lprintf(9, "Decoding %d bytes of HTML\n", input_length);
 
 	do {
 		/* Fill the input buffer */
 		if ( (done_reading == 0) && (strlen(inbuf) < 128) ) {
 
-			/* copy from the input buffer */
-			lprintf(9, "input loop\n");
-			this_read = strlen(readptr);
-			if (this_read > 127) this_read = 127;
-			lprintf(9, "%d bytes\n", this_read);
-			for (i=0; i<this_read; ++i) {
+			/* FIX ... genericize this */
+			ch = *inputmsg++;
+			if (ch > 0) {
 				inbuf[strlen(inbuf)+1] = 0;
-				inbuf[strlen(inbuf)] = readptr[0];
-				++readptr;
+				inbuf[strlen(inbuf)] = ch;
+			} 
+			else {
+				done_reading = 1;
 			}
 
-			if (strlen(readptr)==0) done_reading = 1;
-		}
-		else {
-			lprintf(9, "skipped input loop\n");
 		}
 
 		/* Do some parsing */
-		lprintf(9, "parse loop\n");
 		if (strlen(inbuf)>0) {
 
 		    /* Fold in all the spacing */
-			lprintf(9, "spacing loop\n");
 		    for (i=0; i<strlen(inbuf); ++i) {
 			if (inbuf[i]==10) inbuf[i]=32;
 			if (inbuf[i]==13) inbuf[i]=32;
@@ -89,19 +76,16 @@ char *html_to_ascii(char *inputmsg, int screenwidth) {
 				strcpy(&inbuf[i], &inbuf[i+1]);
 		    }
 
-			lprintf(9, "foo loop\n");
 		    for (i=0; i<strlen(inbuf); ++i) {
 
 			ch = inbuf[i];
 
 			if (ch == '<') {
-			lprintf(9, "bar loop\n");
 				++nest;
 				strcpy(tag, "");
 			}
 
 			else if (ch == '>') {
-			lprintf(9, "baz loop\n");
 				if (nest > 0) --nest;
 				
 				if (!strcasecmp(tag, "P")) {
@@ -142,7 +126,7 @@ char *html_to_ascii(char *inputmsg, int screenwidth) {
 
 				else if (!strcasecmp(tag, "HR")) {
 					strcat(outbuf, "\n ");
-					for (i=0; i<screenwidth-2; ++i)
+					for (j=0; j<screenwidth-2; ++j)
 						strcat(outbuf, "-");
 					strcat(outbuf, "\n");
 				}
@@ -174,23 +158,20 @@ char *html_to_ascii(char *inputmsg, int screenwidth) {
 		    strcpy(inbuf, &inbuf[i]);
 		}
 
-		lprintf(9, "checkquepoynte\n");
-
 		/* Convert &; tags to the forbidden characters */
 		if (strlen(outbuf)>0) for (i=0; i<strlen(outbuf); ++i) {
-			lprintf(9, "eek loop\n");
 
 			if (!strncasecmp(&outbuf[i], "&nbsp;", 6)) {
 				outbuf[i] = ' ';
 				strcpy(&outbuf[i+1], &outbuf[i+6]);
 			}
 
-			else if (!strncasecmp(&outbuf[i], "&lb;", 4)) {
+			else if (!strncasecmp(&outbuf[i], "&lt;", 4)) {
 				outbuf[i] = '<';
 				strcpy(&outbuf[i+1], &outbuf[i+4]);
 			}
 
-			else if (!strncasecmp(&outbuf[i], "&rb;", 4)) {
+			else if (!strncasecmp(&outbuf[i], "&gt;", 4)) {
 				outbuf[i] = '>';
 				strcpy(&outbuf[i+1], &outbuf[i+4]);
 			}
@@ -204,23 +185,22 @@ char *html_to_ascii(char *inputmsg, int screenwidth) {
 				strcpy(&outbuf[i+1], &outbuf[i+6]);
 			}
 
-		}
+			else if (!strncasecmp(&outbuf[i], "&copy;", 6)) {
+				outbuf[i] = '(';
+				outbuf[i+1] = 'c';
+				outbuf[i+2] = ')';
+				strcpy(&outbuf[i+3], &outbuf[i+6]);
+			}
 
-		/* Make sure the output buffer is big enough */
-		if ((strlen(outputbuf) + strlen(outbuf) + 2) > output_length) {
-			lprintf(9, "realloc loop\n");
-			output_length = output_length + strlen(outbuf) + 2;
-			outputbuf = reallok(outputbuf, output_length);
 		}
 
 		/* Output any lines terminated with hard line breaks */
-		lprintf(9, "output loop 1\n");
 		do {
 			did_out = 0;
 			if (strlen(outbuf)>0)
 			    for (i = 0; i<strlen(outbuf); ++i) {
 				if ( (i<(screenwidth-2)) && (outbuf[i]=='\n')) {
-					strncat(outputbuf, outbuf, i+1);
+					fwrite(outbuf, i+1, 1, stdout);
 					strcpy(outbuf, &outbuf[i+1]);
 					i = 0;
 					did_out = 1;
@@ -229,27 +209,29 @@ char *html_to_ascii(char *inputmsg, int screenwidth) {
 		} while (did_out);
 
 		/* Add soft line breaks */
-		lprintf(9, "output loop 2\n");
 		if (strlen(outbuf) > (screenwidth - 2)) {
 			rb = (-1);
 			for (i=0; i<(screenwidth-2); ++i) {
 				if (outbuf[i]==32) rb = i;
 			}
 			if (rb>=0) {
-				strncat(outputbuf, outbuf, rb);
-				strcat(outputbuf, "\n");
+				fwrite(outbuf, rb, 1, stdout);
+				fwrite("\n", 1, 1, stdout);
 				strcpy(outbuf, &outbuf[rb+1]);
 			} else {
-				strncat(outputbuf, outbuf, screenwidth-2);
-				strcat(outputbuf, "\n");
+				fwrite(outbuf, screenwidth-2, 1, stdout);
+				fwrite("\n", 1, 1, stdout);
 				strcpy(outbuf, &outbuf[screenwidth-2]);
 			}
 		}
 
 	} while (done_reading == 0);
-	lprintf(9, "output loop 3\n");
-	strncat(outputbuf, outbuf, strlen(outbuf));
-	strcat(outputbuf, "\n");
+	fwrite(outbuf, strlen(outbuf), 1, stdout);
+	fwrite("\n", 1, 1, stdout);
 
-	return outputbuf;
+	inptr = mallok(100);
+	strcpy(inptr, "This is eekish.\n");
+	return inptr;
+
 }
+
