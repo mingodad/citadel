f1da50d33e281424752062a034264a6c136856cb
Art Cancro <ajc@citadel.org>
1999-09-23 03:07:56 +0000

    * The vCard 'class' is now linked into the server, though it's not really   functional yet.  Its constructors/destructors are debugged, though.

--- citadel/vcard.c
+++ citadel/vcard.c
@@ -9,28 +9,27 @@
  */
 
 
+#include "sysdep.h"
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <time.h>
 #include <ctype.h>
 #include <string.h>
-
-#define CTDL_VCARD_MAGIC	0xa1f9
-
-/*
- * This data structure represents a vCard object currently in memory.
- */
-struct vCard {
-	int magic;
-	int numprops;
-	struct {
-		char *name;
-		char *value;
-	} *prop;
-};
-
-
-
+#include <errno.h>
+#include <limits.h>
+#include <pthread.h>
+#include <syslog.h>
+#include "citadel.h"
+#include "server.h"
+#include "control.h"
+#include "sysdep_decls.h"
+#include "support.h"
+#include "config.h"
+#include "tools.h"
+#include "vcard.h"
 
 /* 
  * Constructor (empty vCard)
@@ -38,13 +37,88 @@ struct vCard {
 struct vCard *new_vcard() {
 	struct vCard *v;
 
-	v = (struct vCard *) malloc(sizeof(struct vCard));
+	v = (struct vCard *) mallok(sizeof(struct vCard));
 	if (v == NULL) return v;
 
 	v->magic = CTDL_VCARD_MAGIC;
 	v->numprops = 0;
 	v->prop = NULL;
 
+	return v;
+}
+
+
+/*
+ * Constructor (supply serialized vCard)
+ */
+struct vCard *load_vcard(char *vtext) {
+	struct vCard *v;
+	int valid = 0;
+	char *mycopy, *ptr;
+	char *namebuf, *valuebuf;
+	int i;
+	int colonpos, nlpos;
+
+	mycopy = strdoop(vtext);
+	if (mycopy == NULL) return NULL;
+
+	/* First, fix this big pile o' vCard to make it more parseable.
+	 * To make it easier to parse, we convert CRLF to LF, and unfold any
+	 * multi-line fields into single lines.
+	 */
+	for (i=0; i<strlen(mycopy); ++i) {
+		if (!strncmp(&mycopy[i], "\r\n", 2)) {
+			strcpy(&mycopy[i], &mycopy[i+1]);
+		}
+		if ( (mycopy[i]=='\n') && (isspace(mycopy[i+1])) ) {
+			strcpy(&mycopy[i], &mycopy[i+1]);
+		}
+	}
+
+	v = new_vcard();
+	if (v == NULL) return v;
+
+	ptr = mycopy;
+	while (strlen(ptr)>0) {
+		colonpos = (-1);
+		nlpos = (-1);
+		colonpos = pattern2(ptr, ":");
+		nlpos = pattern2(ptr, "\n");
+
+		if (nlpos > colonpos > 0) {
+			namebuf = mallok(colonpos + 1);
+			valuebuf = mallok(nlpos - colonpos + 1);
+			strncpy(namebuf, ptr, colonpos);
+			namebuf[colonpos] = 0;
+			strncpy(valuebuf, &ptr[colonpos+1], nlpos-colonpos-1);
+			valuebuf[nlpos-colonpos-1] = 0;
+
+			if ( (!strcasecmp(namebuf, "begin"))
+			   && (!strcasecmp(valuebuf, "vcard")) )  valid = 1;
+			if ( (!strcasecmp(namebuf, "end"))
+			   && (!strcasecmp(valuebuf, "vcard")) )  valid = 0;
+
+			if (valid) {
+				++v->numprops;
+				v->prop = reallok(v->prop,
+					(v->numprops * sizeof(char *) * 2) );
+				v->prop[v->numprops-1].name = namebuf;
+				v->prop[v->numprops-1].value = valuebuf;
+			}
+			else {
+				phree(namebuf);
+				phree(valuebuf);
+			}
+
+		}
+
+		while ( (*ptr != '\n') && (strlen(ptr)>0) ) {
+			++ptr;
+		}
+		if (*ptr == '\n') ++ptr;
+	}
+
+	phree(mycopy);
 	return v;
 }
 
@@ -59,11 +133,11 @@ void free_vcard(struct vCard *v) {
 	if (v->magic != CTDL_VCARD_MAGIC) return;	/* Self-check */
 	
 	if (v->numprops) for (i=0; i<(v->numprops); ++i) {
-		free(v->prop[i].name);
-		free(v->prop[i].value);
+		phree(v->prop[i].name);
+		phree(v->prop[i].value);
 	}
 
-	if (v->prop != NULL) free(v->prop);
+	if (v->prop != NULL) phree(v->prop);
 	
 	memset(v, 0, sizeof(struct vCard));
 }
---
+++ citadel/vcard.h
@@ -0,0 +1,29 @@
+/*
+ * $Id$
+ *
+ * vCard implementation for Citadel/UX
+ *
+ * Copyright (C) 1999 by Art Cancro
+ * This code is freely redistributable under the terms of the GNU General
+ * Public License.  All other rights reserved.
+ */
+
+
+#define CTDL_VCARD_MAGIC	0xa1f9
+
+/*
+ * This data structure represents a vCard object currently in memory.
+ */
+struct vCard {
+	int magic;
+	int numprops;
+	struct vCardProp {
+		char *name;
+		char *value;
+	} *prop;
+};
+
+
+struct vCard *new_vcard(void);
+struct vCard *load_vcard(char *);
+void free_vcard(struct vCard *);
