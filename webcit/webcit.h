f8e5f597f68a3abd166c80b3b088cdb20e3418da
Art Cancro <ajc@citadel.org>
2000-01-04 15:39:30 +0000

    * oops, forgot to bump the version number in .h and docs

--- webcit/webcit.h
+++ webcit/webcit.h
@@ -3,10 +3,10 @@
 #define SLEEPING		180		/* TCP connection timeout */
 #define WEBCIT_TIMEOUT		900		/* WebCit session timeout */
 #define PORT_NUM		2000		/* port number to listen on */
-#define SERVER			"WebCit v2.10"	/* who's in da house */
+#define SERVER			"WebCit v2.11"	/* who's in da house */
 #define DEVELOPER_ID		0
 #define CLIENT_ID		4
-#define CLIENT_VERSION		210
+#define CLIENT_VERSION		211
 #define DEFAULT_HOST		"localhost"	/* Default Citadel server */
 #define DEFAULT_PORT		"504"
 #define LB			(1)		/* Internal escape chars */
