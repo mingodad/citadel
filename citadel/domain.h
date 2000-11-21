1eb30cecec1d4241db2a223d033bafda95f81af4
Michael Hampton <io_error@uncensored.citadel.org>
2000-11-21 11:12:57 +0000

    * domain.h: changed the HP/UX compatibility code to use defines from    typesize.h for integers of specific bit widths (needed for Solaris, etc)

--- citadel/domain.h
+++ citadel/domain.h
@@ -14,12 +14,14 @@ int getmx(char *mxbuf, char *dest);
 
 /* HP/UX has old include files...these are from arpa/nameser.h */
 
+#include "typesize.h"
+
 #ifndef HFIXEDSZ
 #define HFIXEDSZ	12		/* I hope! */
 #endif
 #ifndef INT16SZ
-#define	INT16SZ		sizeof(int16)
+#define	INT16SZ		sizeof(cit_int16_t)
 #endif
 #ifndef INT32SZ
-#define INT32SZ		sizeof(int32)
+#define INT32SZ		sizeof(cit_int32_t)
 #endif
