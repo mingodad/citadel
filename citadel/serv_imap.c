d558b6e895f7807b9e3f6b8b3ba5f034d29cba4d
Art Cancro <ajc@citadel.org>
2001-03-20 01:33:56 +0000

    * Added the (\NoInferiors) tag to all rooms listed in IMAP.  This made Mozilla   behave very nicely.

--- citadel/serv_imap.c
+++ citadel/serv_imap.c
@@ -474,7 +474,7 @@ void imap_lsub_listroom(struct quickroom *qrbuf, void *data) {
 	if (ra & UA_KNOWN) {
 		imap_mailboxname(buf, sizeof buf, qrbuf);
 		if (imap_mailbox_matches_pattern(pattern, buf)) {
-			cprintf("* LSUB () \"|\" ");
+			cprintf("* LSUB (\\NoInferiors) \"|\" ");
 			imap_strout(buf);
 			cprintf("\r\n");
 		}
@@ -526,7 +526,7 @@ void imap_list_listroom(struct quickroom *qrbuf, void *data) {
 	  || ((ra & UA_GOTOALLOWED) && (ra & UA_ZAPPED))) {
 		imap_mailboxname(buf, sizeof buf, qrbuf);
 		if (imap_mailbox_matches_pattern(pattern, buf)) {
-			cprintf("* LIST () \"|\" ");
+			cprintf("* LIST (\\NoInferiors) \"|\" ");
 			imap_strout(buf);
 			cprintf("\r\n");
 		}
