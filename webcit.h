0c237d9a0f6e4a1105ee9f433d3539683397f017
Art Cancro <ajc@citadel.org>
2001-12-21 06:03:55 +0000

    * 3.20

--- webcit.h
+++ webcit.h
@@ -7,10 +7,10 @@
 #define SLEEPING		180		/* TCP connection timeout */
 #define WEBCIT_TIMEOUT		900		/* WebCit session timeout */
 #define PORT_NUM		2000		/* port number to listen on */
-#define SERVER			"WebCit v3.01"	/* who's in da house */
+#define SERVER			"WebCit v3.20"	/* who's in da house */
 #define DEVELOPER_ID		0
 #define CLIENT_ID		4
-#define CLIENT_VERSION		310
+#define CLIENT_VERSION		320
 #define DEFAULT_HOST		"localhost"	/* Default Citadel server */
 #define DEFAULT_PORT		"504"
 #define LB			(1)		/* Internal escape chars */
