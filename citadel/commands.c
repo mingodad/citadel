301c18ececd151e662ce2e3f69f5617a6b19d0ba
Michael Hampton <io_error@uncensored.citadel.org>
2002-05-14 01:15:54 +0000

    * Idle threshold on the who list is now customizable in the citadel.rc

--- citadel/commands.c
+++ citadel/commands.c
@@ -76,6 +76,7 @@ int rc_remember_passwords;
 int rc_ansi_color;
 int num_urls = 0;
 int rc_prompt_control = 0;
+int rc_idle_threshold = 900;
 char urls[MAXURLS][SIZ];
 char rc_url_cmd[SIZ];
 
@@ -805,6 +806,9 @@ void load_command_set(void)
 		}
 		if (!strncasecmp(buf, "allow_attachments=", 18)) {
 			rc_allow_attachments = atoi(&buf[18]);
+		}
+		if (!strncasecmp(buf, "idle_threshold=", 14)) {
+			rc_idle_threshold = atoi(&buf[14]);
 		}
 		if (!strncasecmp(buf, "remember_passwords=", 19)) {
 			rc_remember_passwords = atoi(&buf[19]);
