b082c184eb2bc968754ecba05415fb8bb977c2ea
Art Cancro <ajc@citadel.org>
2002-10-16 13:46:20 +0000

    * Remove some trace messages

--- citadel/dynloader.c
+++ citadel/dynloader.c
@@ -551,16 +551,11 @@ void PerformSessionHooks(int EventType)
 {
 	struct SessionFunctionHook *fcn;
 
-	TRACE;
 	for (fcn = SessionHookTable; fcn != NULL; fcn = fcn->next) {
-		TRACE;
 		if (fcn->eventtype == EventType) {
-			TRACE;
 			(*fcn->h_function_pointer) ();
 		}
-		TRACE;
 	}
-	TRACE;
 }
 
 void PerformLogHooks(int loglevel, char *logmsg)
