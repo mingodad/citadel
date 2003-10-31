eab6b710a753379d2c75162663a7b05d0b0c3b29
Art Cancro <ajc@citadel.org>
2003-10-31 04:54:52 +0000

    * move index to a symlink

---
+++ ctdlphp/index.php
@@ -0,0 +1,15 @@
+<?PHP
+	include "ctdlheader.php";
+
+	bbs_page_header();
+
+	ctdl_mesg("hello");
+?>
+
+<a href="page2.php">Page Two</a><BR>
+<a href="page3.php">Page Three</a><BR>
+
+<?PHP
+	bbs_page_footer();
+?>
+
