0edef76d13c068cd29bd45ca16e4a504f72cacf0
Art Cancro <ajc@citadel.org>
2004-02-25 23:00:21 +0000

    * Changed the favicon support to LINK tags in the HEAD of static stuff,   to make life easier.  (Thanks to prosthetik for this)

--- webcit/context_loop.c
+++ webcit/context_loop.c
@@ -316,13 +316,6 @@ void context_loop(int sock)
 				"?force_close_session=yes HTTP/1.0");
 	}
 
-	/*
-	 * favicon.ico can't be in the root...
-	 */
-	if (!strncasecmp(buf, "/favicon.ico", 12)) {
-		strcpy(req->line, "GET /static/favicon.ico");
-	}
-
 	/* These are the URL's which may be executed without a
 	 * session cookie already set.  If it's not one of these,
 	 * force the session to close because cookies are
