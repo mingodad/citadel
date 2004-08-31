4714fb20b7b420ed0b184111d86363f5b0a32fe2
Art Cancro <ajc@citadel.org>
2004-08-31 03:02:28 +0000

    * Changed internal version number to 5.23

--- webcit.h
+++ webcit.h
@@ -33,10 +33,10 @@
 #define SLEEPING		180		/* TCP connection timeout */
 #define WEBCIT_TIMEOUT		900		/* WebCit session timeout */
 #define PORT_NUM		2000		/* port number to listen on */
-#define SERVER			"WebCit v5.22"	/* who's in da house */
+#define SERVER			"WebCit v5.23"	/* who's in da house */
 #define DEVELOPER_ID		0
 #define CLIENT_ID		4
-#define CLIENT_VERSION		522		/* This version of WebCit */
+#define CLIENT_VERSION		523		/* This version of WebCit */
 #define MINIMUM_CIT_VERSION	623		/* min required Citadel vers */
 #define DEFAULT_HOST		"localhost"	/* Default Citadel server */
 #define DEFAULT_PORT		"504"
