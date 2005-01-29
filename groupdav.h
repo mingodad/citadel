187a8e2647a0778ae943aab9732a8ee250cc9bc9
Art Cancro <ajc@citadel.org>
2005-01-29 03:59:25 +0000

    * Implemented the GroupDAV DELETE method.

--- groupdav.h
+++ groupdav.h
@@ -3,5 +3,6 @@
 void groupdav_common_headers(void);
 void groupdav_main(struct httprequest *);
 void groupdav_get(char *);
+void groupdav_delete(char *, char *);
 void groupdav_propfind(char *);
 long locate_message_by_uid(char *);
