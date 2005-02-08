56e38f632d27af7f0a5eb5113cb4986c1027ee78
Art Cancro <ajc@citadel.org>
2005-02-08 04:34:02 +0000

    *** empty log message ***

--- webcit/auth.c
+++ webcit/auth.c
@@ -46,7 +46,8 @@ void display_login(char *mesg)
 	char buf[SIZ];
 
 	output_headers(1, 1, 2, 0, 0, 0, 0);
-	wprintf("<div id=\"content\">\n");
+	//wprintf("<div id=\"content\">\n");
+	wprintf("<div>\n");
 
 	if (mesg != NULL) if (strlen(mesg) > 0) {
 		stresc(buf, mesg, 0, 0);
