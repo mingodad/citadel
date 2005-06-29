75781b35b039656ce499b2f47490690385b70cf1
Art Cancro <ajc@citadel.org>
2005-06-29 17:55:32 +0000

    * Any HTTP method other than GET or POST is now routed directly to the   GroupDAV code.  This will eventually allow requests such as "PROPFIND /"   to work. * GroupDAV requests not beginning with "/groupdav" now automatically have   that prefix prepended to them.

--- webcit/groupdav.h
+++ webcit/groupdav.h
@@ -6,6 +6,7 @@ void groupdav_get(char *);
 void groupdav_put(char *, char *, char *, char *);
 void groupdav_delete(char *, char *);
 void groupdav_propfind(char *);
+void groupdav_options(char *);
 long locate_message_by_uid(char *);
 void groupdav_folder_list(void);
 void euid_escapize(char *, char *);
