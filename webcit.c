ef2007c19661ff1f0073331ab68e2a07952ead66
Art Cancro <ajc@citadel.org>
2005-07-19 14:23:04 +0000

    * mime_parser.c: don't default Content-type: to text/plain, because when   an HTTP POST operation is performed, the content type is expected to   be empty.

--- webcit.c
+++ webcit.c
@@ -817,8 +817,8 @@ void extract_action(char *actbuf, char *cmdbuf)
 
 
 void upload_handler(char *name, char *filename, char *partnum, char *disp,
-			void *content, char *cbtype, char *cbcharset, size_t length,
-			char *encoding, void *userdata)
+			void *content, char *cbtype, char *cbcharset,
+			size_t length, char *encoding, void *userdata)
 {
 	struct urlcontent *u;
 
