4ec0ace183a6a7b275aeb3564aef28a59a84367c
Art Cancro <ajc@citadel.org>
2005-10-07 19:39:51 +0000

    re-added binary

---
+++ static/wclib.js
@@ -0,0 +1,48 @@
+//
+// $Id: wclib.js,v 625.2 2005/09/18 04:04:32 ajc Exp $
+//
+// JavaScript function library for WebCit
+//
+//
+
+
+//
+// This code handles the popups for instant messages.
+//
+
+var browserType;
+
+if (document.layers) {browserType = "nn4"}
+if (document.all) {browserType = "ie"}
+if (window.navigator.userAgent.toLowerCase().match("gecko")) {
+	browserType= "gecko"
+}
+
+function hide_page_popup() {
+	if (browserType == "gecko" )
+		document.poppedLayer = eval('document.getElementById(\'page_popup\')');
+	else if (browserType == "ie")
+		document.poppedLayer = eval('document.all[\'page_popup\']');
+	else
+		document.poppedLayer = eval('document.layers[\'`page_popup\']');
+
+	document.poppedLayer.style.visibility = "hidden";
+}
+
+function hide_imsg_popup() {
+	if (browserType == "gecko" )
+		document.poppedLayer = eval('document.getElementById(\'important_message\')');
+	else if (browserType == "ie")
+		document.poppedLayer = eval('document.all[\'important_message\']');
+	else
+		document.poppedLayer = eval('document.layers[\'`important_message\']');
+
+	document.poppedLayer.style.visibility = "hidden";
+}
+
+// This function activates the ajax-powered recipient autocompleters on the message entry screen.
+function activate_entmsg_autocompleters() {
+	new Ajax.Autocompleter('cc_id', 'cc_name_choices', '/cc_autocomplete', {} );
+	new Ajax.Autocompleter('bcc_id', 'bcc_name_choices', '/bcc_autocomplete', {} );
+	new Ajax.Autocompleter('recp_id', 'recp_name_choices', '/recp_autocomplete', {} );
+}
