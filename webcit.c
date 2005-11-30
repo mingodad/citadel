89889de45002748e786e82fdfe4bff2647c0e0f9
Art Cancro <ajc@citadel.org>
2005-11-30 04:57:21 +0000

    commented out some logging

--- webcit.c
+++ webcit.c
@@ -832,11 +832,11 @@ void ajax_servcmd(void)
 
 	begin_ajax_response();
 
-	lprintf(9, "Sending cmd: %s\n", bstr("g_cmd"));
+	/* lprintf(9, "Sending cmd: %s\n", bstr("g_cmd")); */
 	serv_printf("%s", bstr("g_cmd"));
 	serv_getln(buf, sizeof buf);
 	wprintf("%s\n", buf);
-	lprintf(9, "   Response: %s\n", buf);
+	/* lprintf(9, "   Response: %s\n", buf); */
 
 	if (buf[0] == '8') {
 		serv_printf("\n\n000");
