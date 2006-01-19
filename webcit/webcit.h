72fd6efbc074b3272befc88dfd2415d5a2af84a9
Art Cancro <ajc@citadel.org>
2006-01-19 04:09:43 +0000

    THIS IS 6.71

--- webcit/webcit.h
+++ webcit/webcit.h
@@ -119,10 +119,10 @@
 #define SLEEPING		180		/* TCP connection timeout */
 #define WEBCIT_TIMEOUT		900		/* WebCit session timeout */
 #define PORT_NUM		2000		/* port number to listen on */
-#define SERVER			"WebCit v6.70"	/* who's in da house */
+#define SERVER			"WebCit v6.71"	/* who's in da house */
 #define DEVELOPER_ID		0
 #define CLIENT_ID		4
-#define CLIENT_VERSION		670		/* This version of WebCit */
+#define CLIENT_VERSION		671		/* This version of WebCit */
 #define MINIMUM_CIT_VERSION	670		/* min required Citadel ver. */
 #define DEFAULT_HOST		"localhost"	/* Default Citadel server */
 #define DEFAULT_PORT		"504"
