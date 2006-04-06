79d535772455fb960dcbdbd0325d954d5e2c6fb6
Wilfried Göesgens <willi@citadel.org>
2006-04-06 22:05:35 +0000

    more modern version.

--- webcit/autogen.sh
+++ webcit/autogen.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # tool to generate the automaticaly provided stuff 
 svn log >ChangeLog
-intltoolize --force 2>&1 |grep -v "You should update your 'aclocal.m4' by running aclocal."
+#intltoolize --force 2>&1 |grep -v "You should update your 'aclocal.m4' by running aclocal."
+autopoint --force
 autoreconf -i --force 2>&1|grep -v 'warning: underquoted definition' 
