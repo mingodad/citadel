4066bba62d1e1963558a0443406b1db8c74db9f5
Art Cancro <ajc@citadel.org>
2006-04-07 17:26:40 +0000

    Define _GNU_SOURCE in our hacked-together config.h

--- config.h
+++ config.h
@@ -2,6 +2,7 @@
  *	Ugly hack to make the non-automake build work without source changes
  */
 
+#define _GNU_SOURCE
 #define EDITORDIR	WEBCITDIR "/tiny_mce"
 #define RUNDIR		WEBCITDIR
 #define BASEDIR		WEBCITDIR
