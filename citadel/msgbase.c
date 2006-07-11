a784b963e59f506e16bd9829cb71ba625fddc81e
Art Cancro <ajc@citadel.org>
2006-07-11 17:16:14 +0000

    When generating Message ID's, pad with leading zeroes to ten places in order to thwart a b0rken SpamAssassin rule which assumes that less than five characters to the left of the @ means a message is spam.

--- citadel/msgbase.c
+++ citadel/msgbase.c
@@ -2029,7 +2029,7 @@ long send_message(struct CtdlMessage *msg) {
 
 	/* Get a new message number */
 	newmsgid = get_new_message_number();
-	snprintf(msgidbuf, sizeof msgidbuf, "%ld@%s", newmsgid, config.c_fqdn);
+	snprintf(msgidbuf, sizeof msgidbuf, "%010ld@%s", newmsgid, config.c_fqdn);
 
 	/* Generate an ID if we don't have one already */
 	if (msg->cm_fields['I']==NULL) {
