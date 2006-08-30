0a5fd8d642117d8b318365959fd6c50aecfbaa2b
Art Cancro <ajc@citadel.org>
2006-08-30 03:20:01 +0000

    * Internal version number is now 6.84 * Require Citadel server v6.84 also, for new functionality

--- webcit.h
+++ webcit.h
@@ -122,11 +122,11 @@ extern locale_t wc_locales[];
 #define SLEEPING		180		/* TCP connection timeout */
 #define WEBCIT_TIMEOUT		900		/* WebCit session timeout */
 #define PORT_NUM		2000		/* port number to listen on */
-#define SERVER			"WebCit v6.82"	/* who's in da house */
+#define SERVER			"WebCit v6.84"	/* who's in da house */
 #define DEVELOPER_ID		0
 #define CLIENT_ID		4
-#define CLIENT_VERSION		682		/* This version of WebCit */
-#define MINIMUM_CIT_VERSION	673		/* min required Citadel ver. */
+#define CLIENT_VERSION		684		/* This version of WebCit */
+#define MINIMUM_CIT_VERSION	684		/* min required Citadel ver. */
 #define DEFAULT_HOST		"localhost"	/* Default Citadel server */
 #define DEFAULT_PORT		"504"
 #define LB			(1)		/* Internal escape chars */
