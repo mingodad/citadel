3d850e6005db56f27773226ce3907ea86da9e484
Wilfried Göesgens <willi@citadel.org>
2006-10-03 14:58:55 +0000

    * inject the welcome mail into the lobby of the local system.

---
+++ citadel/sendwelcomemail.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+export FOO=2600908b3f21ae7f692b973ed26e212d
+export WELCOMEHTML=docs/welcomemail.html
+export WELCOMETXT=docs/welcomemail.txt
+export FROM=room_citadel_stats@uncensored.citadel.org
+export TO=room_lobby
+(
+    printf "MIME-Version: 1.0\r\nContent-Type: multipart/alternative; \r\n boundary=$FOO\r\n\r\nThis is a multi-part message in MIME format.\r\n\r\n--$FOO\r\nContent-Type: text/plain; charset=utf-8\r\nContent-Transfer-Encoding: quoted-printable\r\n\r\n"; 
+    cat $WELCOMETXT
+    printf "\r\n\r\n--$FOO\r\nContent-Type: text/html; charset=US-ASCII\r\nContent-Transfer-Encoding: quoted-printable\r\n\r\n"
+    cat $WELCOMEHTML; 
+    printf "\r\n\r\n--$FOO--\r\n\r\n") | \
+    citmail -bm -r "$FROM" "$TO"
