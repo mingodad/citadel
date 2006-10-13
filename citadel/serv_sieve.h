e6d74430697dd6bc094672e00655d48b5b7046a3
Art Cancro <ajc@citadel.org>
2006-10-13 16:26:55 +0000

    Added function prototypes for the msiv_*() API calls

--- citadel/serv_sieve.h
+++ citadel/serv_sieve.h
@@ -34,12 +34,18 @@ struct ctdl_sieve {
 
 #endif /* HAVE_LIBSIEVE */
 
-
+/* If you change this string you will break all of your Sieve configs. */
+#define CTDLSIEVECONFIGSEPARATOR	"\n-=<CtdlSieveConfigSeparator>=-\n"
 
 extern struct RoomProcList *sieve_list;
 
 void sieve_queue_room(struct ctdlroom *);
 void perform_sieve_processing(void);
 
-/* If you change this string you will break all of your Sieve configs. */
-#define CTDLSIEVECONFIGSEPARATOR	"\n-=<CtdlSieveConfigSeparator>=-\n"
+void msiv_load(struct sdm_userdata *u);
+void msiv_store(struct sdm_userdata *u);
+int msiv_setactive(struct sdm_userdata *u, char *script_name);
+char *msiv_getscript(struct sdm_userdata *u, char *script_name);
+int msiv_deletescript(struct sdm_userdata *u, char *script_name);
+void msiv_putscript(struct sdm_userdata *u, char *script_name, char *script_content);
+
