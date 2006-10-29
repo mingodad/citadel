2929f14e3f54651ed44617443633a233be0e3b24
Art Cancro <ajc@citadel.org>
2006-10-29 18:47:13 +0000

    Minor edit to sieve screen

--- sieve.c
+++ sieve.c
@@ -28,7 +28,33 @@ void display_sieve(void)
 		"<table border=0 width=100%% bgcolor=\"#FFFFFF\">"
 		"<tr><td valign=top>\n");
 
-	wprintf("FIXME there is nothing here yet.");
+
+	wprintf("<form method=\"post\" action=\"save_sieve\">\n");
+
+	wprintf("</form>\n");
+
+	wprintf(_("When new mail arrives: "));
+        wprintf("<select name=\"bigaction\" size=1>\n");
+
+	wprintf("<option value=\"0\">");
+	wprintf(_("Leave it in my inbox without filtering"));
+	wprintf("</option>\n");
+
+	wprintf("<option value=\"1\">");
+	wprintf(_("Filter it according to rules selected below"));
+	wprintf("</option>\n");
+
+	wprintf("<option value=\"2\">");
+	wprintf(_("Filter it through a manually edited script (advanced users only)"));
+	wprintf("</option>\n");
+
+	wprintf("</select>");
+
+	wprintf("<div align=\"center\"><br>");
+	wprintf("<input type=\"submit\" name=\"ok_button\" value=\"%s\">", _("Save changes"));
+	wprintf("&nbsp;");
+	wprintf("<input type=\"submit\" name=\"cancel_button\" value=\"%s\">\n", _("Cancel"));
+	wprintf("</div></form>\n");
 
 	wprintf("</td></tr></table></div>\n");
 	wDumpContent(1);
