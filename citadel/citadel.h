13be2a2f50573b9b50c9e754599065da69431b99
Wilfried Göesgens <willi@citadel.org>
2006-12-28 21:19:39 +0000

    * bumped verison to 7.02 * added copying file * do boostrap before grepping the version * add the version jump to the debian changelog

--- citadel/citadel.h
+++ citadel/citadel.h
@@ -47,7 +47,7 @@ extern "C" {
  * usually more strict because you're not really supposed to dump/load and
  * upgrade at the same time.
  */
-#define REV_LEVEL	701		/* This version */
+#define REV_LEVEL	702		/* This version */
 #define REV_MIN		591		/* Oldest compatible database */
 #define EXPORT_REV_MIN	701		/* Oldest compatible export files */
 
