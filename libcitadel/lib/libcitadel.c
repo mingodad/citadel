731e27c55ab4224bbc40fed4e15bf86077384646
Art Cancro <ajc@citadel.org>
2007-11-09 20:12:20 +0000

    Added the MIME parser to libcitadel.

--- libcitadel/lib/libcitadel.c
+++ libcitadel/lib/libcitadel.c
@@ -3,7 +3,18 @@
  *
  */
 
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <ctype.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <errno.h>
 #include "libcitadel.h"
+
 
 char *libcitadel_version_string(void) {
 	return "$Id$";
--- libcitadel/lib/libcitadel.h
+++ libcitadel/lib/libcitadel.h
@@ -3,4 +3,60 @@
  *
  */
 
+/*
+ * Here's a bunch of stupid magic to make the MIME parser portable.
+ */
+#ifndef SIZ
+#define SIZ	4096
+#endif
+
+/*
+ * Misc declarations
+ */
+
 char *libcitadel_version_string(void);
+
+
+/*
+ * MIME parser declarations
+ */
+
+void extract_key(char *target, char *source, char *key);
+
+void mime_parser(char *content_start, char *content_end,
+		void (*CallBack)
+			(char *cbname,
+			char *cbfilename,
+			char *cbpartnum,
+			char *cbdisp,
+			void *cbcontent,
+			char *cbtype,
+			char *cbcharset,
+			size_t cblength,
+			char *cbencoding,
+			void *cbuserdata),
+		void (*PreMultiPartCallBack)
+			(char *cbname,
+			char *cbfilename,
+			char *cbpartnum,
+			char *cbdisp,
+			void *cbcontent,
+			char *cbtype,
+			char *cbcharset,
+			size_t cblength,
+			char *cbencoding,
+			void *cbuserdata),
+		void (*PostMultiPartCallBack)
+			(char *cbname,
+			char *cbfilename,
+			char *cbpartnum,
+			char *cbdisp,
+			void *cbcontent,
+			char *cbtype,
+			char *cbcharset,
+			size_t cblength,
+			char *cbencoding,
+			void *cbuserdata),
+		void *userdata,
+		int dont_decode
+		);
