bec0588b1f614d9db83309dd56449c73f6deef63
Art Cancro <ajc@citadel.org>
2007-11-15 16:19:24 +0000

    Updated version number to 7.24

--- citadel/citadel.h
+++ citadel/citadel.h
@@ -47,7 +47,7 @@ extern "C" {
  * usually more strict because you're not really supposed to dump/load and
  * upgrade at the same time.
  */
-#define REV_LEVEL	723		/* This version */
+#define REV_LEVEL	724		/* This version */
 #define REV_MIN		591		/* Oldest compatible database */
 #define EXPORT_REV_MIN	722		/* Oldest compatible export files */
 
