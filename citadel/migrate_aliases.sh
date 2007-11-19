f08f0bc29d1d25e260899818949d8992a2d62924
Wilfried Göesgens <willi@citadel.org>
2007-11-19 22:36:39 +0000

    * add a script that tries to migrate the systems /etc/aliases file if existant.

---
+++ citadel/migrate_aliases.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+CITALIAS=$1
+if test -f /etc/aliases; then
+    cat /etc/aliases | \
+	sed -e "s; *;;g" \
+            -e "s;\t*;;g" | \
+	grep -v ^root: | \
+	grep -v ^# | \
+	sed -e "s;:root;,room_aide;" \
+            -e "s;:;,;" >/tmp/aliases
+
+    NLINES=`cat /tmp/aliases|wc -l`
+    
+    for ((i=1; i <= $NLINES; i++)); do 
+	ALIAS=`head -n $i /tmp/aliases |tail -n 1`
+	ORG=`echo $ALIAS|sed "s;,.*;;"`
+	if grep $ORG $CITALIAS; then
+	    echo "Ignoring Alias $ORG as its alreday there"
+	else
+	    echo "$ALIAS" >>$CITALIAS
+	fi
+    done
+else
+    echo "no /etc/aliases found."
+fi 
