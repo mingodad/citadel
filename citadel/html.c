abc21bd787083f3d19b3e01abd0a52d73e9cc0ea
Art Cancro <ajc@citadel.org>
2008-02-04 18:38:35 +0000

    html.c support the '&acute;' entity tag

--- citadel/html.c
+++ citadel/html.c
@@ -437,6 +437,11 @@ char *html_to_ascii(char *inputmsg, int msglen, int screenwidth, int do_citaform
 				strcpy(&outbuf[i+1], &outbuf[i+7]);
 			}
 
+			else if (!strncasecmp(&outbuf[i], "&acute;", 7)) {
+				outbuf[i] = '\'';
+				strcpy(&outbuf[i+1], &outbuf[i+7]);
+			}
+
 			/* two-digit decimal equivalents */
 			else if ((!strncmp(&outbuf[i], "&#", 2))
 			      && (outbuf[i+4] == ';') ) {
