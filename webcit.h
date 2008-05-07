a10ff8146aad6a3dde517c65189114fdd8df5f5e
Wilfried Göesgens <willi@citadel.org>
2008-05-07 06:56:27 +0000

    * require libcitadel 112

--- webcit.h
+++ webcit.h
@@ -126,7 +126,7 @@ extern locale_t wc_locales[];
 #define CLIENT_ID		4
 #define CLIENT_VERSION		735		/* This version of WebCit */
 #define MINIMUM_CIT_VERSION	730		/* min required Citadel ver */
-#define	LIBCITADEL_MIN		111		/* min required libcitadel ver */
+#define	LIBCITADEL_MIN		112		/* min required libcitadel ver */
 #define DEFAULT_HOST		"localhost"	/* Default Citadel server */
 #define DEFAULT_PORT		"504"
 #define LB			(1)		/* Internal escape chars */
