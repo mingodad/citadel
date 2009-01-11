a659ab73f8435b0aadfec50cf711765b0e6684cd
Art Cancro <ajc@citadel.org>
2009-01-11 05:23:33 +0000

    * messages.c: Msg->MsgBody->msgnum = msgnum;   This needs review by dothebart to see if there are any side effects

--- webcit/messages.c
+++ webcit/messages.c
@@ -88,6 +88,7 @@ int read_message(StrBuf *Target, const char *tmpl, long tmpllen, long msgnum, in
 	Msg->PartNum = PartNum;
 	Msg->MsgBody =  (wc_mime_attachment*) malloc(sizeof(wc_mime_attachment));
 	memset(Msg->MsgBody, 0, sizeof(wc_mime_attachment));
+	Msg->MsgBody->msgnum = msgnum;
 	FoundCharset = NewStrBuf();
 	while ((StrBuf_ServGetln(Buf)>=0) && !Done) {
 		if ( (StrLength(Buf)==3) && 
