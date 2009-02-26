6b1d7ed0d6bf01e709f31e103e888ccf42153448
Wilfried Göesgens <willi@citadel.org>
2009-02-26 22:55:28 +0000

    * cleanup * fix BSD non-preset locale case to C

--- webcit/wc_gettext.h
+++ webcit/wc_gettext.h
@@ -15,5 +15,4 @@ void offer_languages(StrBuf *Target, int nArgs, WCTemplateToken *Token, void *Co
 void set_selected_language(const char *);
 void go_selected_language(void);
 void stop_selected_language(void);
-void preset_locale(void);
 void httplang_to_locale(StrBuf *LocaleString);
