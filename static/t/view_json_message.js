6cf33a815f8e58109023fed8514552563f771ba3
Wilfried Göesgens <willi@citadel.org>
2009-07-07 20:09:57 +0000

    * first draft of json message template; thanks to Jan Kus / RailsLove.com

---
+++ static/t/view_json_message.js
@@ -0,0 +1,18 @@
+{"message": {
+	"from" : {
+		"name" : "<?MAIL:SUMM:FROM("J")>",
+		"email": "<?MAIL:SUMM:RFCA>"
+	},
+	"date" : "<?MAIL:SUMM:DATEFULL>",
+	"to" : {
+		"email" : "<?MAIL:SUMM:TO("J")>"
+	},
+	"cc" : {
+		"email" : "<?MAIL:SUMM:CCCC("J")>"
+	},
+	"subject" : "<?MAIL:SUMM:SUBJECT("J")>"
+  },
+	"body" : "<?MAIL:BODY("J")>",
+	"attachments" : [<?ITERATE("MAIL:MIME:ATTACH:LINKS", "view_json_message_list_attach")>],
+	"submessages" : [<?ITERATE("MAIL:MIME:ATTACH:SUBMESSAGES", "view_json_message_inline_attach")>]
+}
