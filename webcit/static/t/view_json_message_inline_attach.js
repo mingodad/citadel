dcfbe351282f76cc76f8a855aace704793c09377
Wilfried Göesgens <willi@citadel.org>
2009-07-07 20:09:57 +0000

    * first draft of json message template; thanks to Jan Kus / RailsLove.com

---
+++ webcit/static/t/view_json_message_inline_attach.js
@@ -0,0 +1,7 @@
+{
+	"filename" : "<?MAIL:MIME:FILENAME("X")>",
+	"size" : "<?MAIL:MIME:LENGTH>",
+	"content-type" : "<?MAIL:MIME:CONTENTTYPE>",
+	"url" : "mimepart_download/<?MAIL:MIME:MSGNUM>/<?MAIL:MIME:PARTNUM>/<?MAIL:MIME:FILENAME("U")>",
+	"mime-data" : "<?MAIL:MIME:DATA>"
+},	
