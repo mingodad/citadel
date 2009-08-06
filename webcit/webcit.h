5da9caa44b6fb2f963ba2d8960c74d2f508d6db4
Art Cancro <ajc@citadel.org>
2009-08-06 05:50:43 +0000

    * Version number to 7.61 on all components

--- webcit/webcit.h
+++ webcit/webcit.h
@@ -112,9 +112,9 @@
 #define PORT_NUM		2000		/* port number to listen on */
 #define DEVELOPER_ID		0
 #define CLIENT_ID		4
-#define CLIENT_VERSION		760		/* This version of WebCit */
-#define MINIMUM_CIT_VERSION	744		/* min required Citadel ver */
-#define	LIBCITADEL_MIN		744		/* min required libcitadel ver */
+#define CLIENT_VERSION		761		/* This version of WebCit */
+#define MINIMUM_CIT_VERSION	761		/* min required Citadel ver */
+#define	LIBCITADEL_MIN		761		/* min required libcitadel ver */
 #define DEFAULT_HOST		"localhost"	/* Default Citadel server */
 #define DEFAULT_PORT		"504"
 #define TARGET			"webcit01"	/* Target for inline URL's */
