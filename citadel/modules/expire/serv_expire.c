42bbd7bbac12ceb76019f18b1c0c38b9f42c918e
Art Cancro <ajc@citadel.org>
2009-10-11 03:33:51 +0000

    * Checked in some test code and logging to determine whether a bad cast/copy is the source of an openid bug

--- citadel/modules/expire/serv_expire.c
+++ citadel/modules/expire/serv_expire.c
@@ -816,6 +816,13 @@ int PurgeStaleOpenIDassociations(void) {
 		if (cdboi->len > sizeof(long)) {
 			long usernum;
 			usernum = ((long)*(cdboi->ptr));
+			/* FIXME two different things here, trying to figure out whether this is the
+			 * source of a bug.
+			 */
+			CtdlLogPrintf(CTDL_DEBUG, "#1 Evaluating openid association for user %ld\n", usernum);
+			usernum = 0;
+			memcpy(&usernum, cdboi->ptr, sizeof(long));
+			CtdlLogPrintf(CTDL_DEBUG, "#2 Evaluating openid association for user %ld\n", usernum);
 			if (getuserbynumber(&usbuf, usernum) != 0) {
 				deleteme = strdup(cdboi->ptr + sizeof(long)),
 				Put(keys, deleteme, strlen(deleteme), deleteme, generic_free_handler);
