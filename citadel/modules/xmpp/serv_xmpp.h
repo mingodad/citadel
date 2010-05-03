22e65c7b52584ad3ed4a20a8d40d4a018b52e345
Art Cancro <ajc@citadel.org>
2010-05-03 21:19:15 +0000

    * New utility function xmpp_is_visible(), used to determine whether any given session in the wholist is visible to the current user and capable of receiving instant messages.  This eliminates duplicate code in three different places. * The XMPP mortuary is functionally complete and does appear to be working properly.  Upon connecting, an XMPP client now receives 'buddy delete' messages for any buddy he has ever seen, minus the ones who are online right now.  This should effectively eliminate the accumulation of 'ghost' buddies on large public access systems like Uncensored.

--- citadel/modules/xmpp/serv_xmpp.h
+++ citadel/modules/xmpp/serv_xmpp.h
@@ -83,3 +83,4 @@ void xmpp_send_message(char *, char *);
 void xmpp_non_sasl_authenticate(char *, char *, char *, char *);
 void xmpp_massacre_roster(void);
 void xmpp_delete_old_buddies_who_no_longer_exist_from_the_client_roster(void);
+int xmpp_is_visible(struct CitContext *cptr);
