6df65ad98e4aca52a84205a58a4eb961d8db2b3f
Wilfried Göesgens <willi@citadel.org>
2010-07-05 20:42:26 +0000

    * specify utf-8 as charset

--- webcit/po/create-pot.sh
+++ webcit/po/create-pot.sh
@@ -3,6 +3,7 @@
 echo Updating webcit.pot from strings in the source code ...
 xgettext \
 	--copyright-holder='The Citadel Project - http://www.citadel.org' \
+        --from-code='utf-8' \
 	-k_ \
 	-o webcit.pot \
 	../*.c ../static/t/*.html
