5b548e80fabb16f3f05c738dfbcd804849daf4e7
Art Cancro <ajc@citadel.org>
2010-07-21 17:37:10 +0000

    * Version number to 782.  NOT A RELEASE

--- citadel/citadel.h
+++ citadel/citadel.h
@@ -53,7 +53,7 @@ extern "C" {
  * usually more strict because you're not really supposed to dump/load and
  * upgrade at the same time.
  */
-#define REV_LEVEL	781		/* This version */
+#define REV_LEVEL	782		/* This version */
 #define REV_MIN		591		/* Oldest compatible database */
 #define EXPORT_REV_MIN	760		/* Oldest compatible export files */
 #define LIBCITADEL_MIN	781		/* Minimum required version of libcitadel */
