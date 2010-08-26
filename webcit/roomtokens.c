525baa13d0120ebdd600da7f0af08a1d731d1304
Wilfried Göesgens <willi@citadel.org>
2010-08-26 21:51:32 +0000

    * shuffle arround the whole room stuff. we now have:  * roomops.c : handlers; load / save / edit / goto etc. rooms, floors  * roomlist.c : load lists from the server, sort them.  * roomviews.c : view properties  * roomtoknes.c : room & floor tokens & conditionals

---
+++ webcit/roomtokens.c
@@ -0,0 +1,593 @@
+/*
+ * $Id$
+ * Lots of different room-related operations.
+ */
+
+#include "webcit.h"
+#include "webserver.h"
+
+
+/*
+ * Embed the room banner
+ *
+ * got			The information returned from a GOTO server command
+ * navbar_style 	Determines which navigation buttons to display
+ *
+ */
+void tmplput_roombanner(StrBuf *Target, WCTemplputParams *TP)
+{
+	wcsession *WCC = WC;
+	
+	/* refresh current room states... */
+	/* dosen't work??? gotoroom(NULL); */
+	wc_printf("<div id=\"banner\">\n");
+
+	/* The browser needs some information for its own use */
+	wc_printf("<script type=\"text/javascript\">	\n"
+		  "	room_is_trash = %d;		\n"
+		  "</script>\n",
+		  ((WC->CurRoom.RAFlags & UA_ISTRASH) != 0)
+		);
+
+	/*
+	 * If the user happens to select the "make this my start page" link,
+	 * we want it to remember the URL as a "/dotskip" one instead of
+	 * a "skip" or "gotonext" or something like that.
+	 */
+	if (WCC->Hdr->this_page == NULL) {
+		WCC->Hdr->this_page = NewStrBuf();
+	}
+	StrBufPrintf(WCC->Hdr->this_page, 
+		     "dotskip?room=%s",
+		     ChrPtr(WC->CurRoom.name)
+		);
+
+	do_template("roombanner", NULL);
+	/* roombanner contains this for mobile */
+	if (WC->is_mobile)
+		return;
+
+	do_template("navbar", NULL);
+	wc_printf("</div>\n");
+}
+
+
+/*******************************************************************************
+ ********************** FLOOR Tokens *******************************************
+ ******************************************************************************/
+void tmplput_FLOOR_ID(StrBuf *Target, WCTemplputParams *TP) 
+{
+	Floor *myFloor = (Floor *)CTX;
+
+	StrBufAppendPrintf(Target, "%d", myFloor->ID);
+}
+void tmplput_ROOM_FLOORID(StrBuf *Target, WCTemplputParams *TP) 
+{
+	folder *Folder = (folder *)CTX;
+	StrBufAppendPrintf(Target, "%d", Folder->floorid);
+}
+void tmplput_ROOM_FLOOR_ID(StrBuf *Target, WCTemplputParams *TP) 
+{
+	folder *Folder = (folder *)CTX;
+	const Floor *pFloor = Folder->Floor;
+
+	if (pFloor == NULL)
+		return;
+
+	StrBufAppendPrintf(Target, "%d", pFloor->ID);
+}
+
+void tmplput_ROOM_FLOOR_NAME(StrBuf *Target, WCTemplputParams *TP) 
+{
+	folder *Folder = (folder *)CTX;
+	const Floor *pFloor = Folder->Floor;
+
+	if (pFloor == NULL)
+		return;
+
+	StrBufAppendTemplate(Target, TP, pFloor->Name, 0);
+}
+void tmplput_ThisRoomFloorName(StrBuf *Target, WCTemplputParams *TP) 
+{
+	wcsession *WCC = WC;
+	folder *Folder = &WCC->CurRoom;
+	const Floor *pFloor;
+
+	if (Folder == NULL)
+		return;
+
+	pFloor = Folder->Floor;
+	if (pFloor == NULL)
+		return;
+
+	StrBufAppendTemplate(Target, TP, pFloor->Name, 0);
+}
+void tmplput_FLOOR_NAME(StrBuf *Target, WCTemplputParams *TP) 
+{
+	Floor *myFloor = (Floor *)CTX;
+
+	StrBufAppendTemplate(Target, TP, myFloor->Name, 0);
+}
+
+void tmplput_FLOOR_NROOMS(StrBuf *Target, WCTemplputParams *TP) 
+{
+	Floor *myFloor = (Floor *)CTX;
+
+	StrBufAppendPrintf(Target, "%d", myFloor->NRooms);
+}
+void tmplput_ROOM_FLOOR_NROOMS(StrBuf *Target, WCTemplputParams *TP) 
+{
+	folder *Folder = (folder *)CTX;
+	const Floor *pFloor = Folder->Floor;
+
+	if (pFloor == NULL)
+		return;
+	StrBufAppendPrintf(Target, "%d", pFloor->NRooms);
+}
+int ConditionalFloorHaveNRooms(StrBuf *Target, WCTemplputParams *TP)
+{
+	Floor *MyFloor = (Floor *)CTX;
+	int HaveN;
+
+	HaveN = GetTemplateTokenNumber(Target, TP, 0, 0);
+
+	return HaveN == MyFloor->NRooms;
+}
+
+int ConditionalFloorIsRESTSubFloor(StrBuf *Target, WCTemplputParams *TP)
+{
+	wcsession  *WCC = WC;
+	Floor *MyFloor = (Floor *)CTX;
+	/** if we have dav_depth the client just wants the subfloors */
+	if ((WCC->Hdr->HR.dav_depth == 1) && 
+	    (GetCount(WCC->Directory) == 0))
+		return 1;
+	return WCC->CurrentFloor == MyFloor;
+}
+int ConditionalFloorIsSUBROOM(StrBuf *Target, WCTemplputParams *TP)
+{
+	wcsession  *WCC = WC;
+	Floor *MyFloor = (Floor *)CTX;
+
+	return WCC->CurRoom.floorid == MyFloor->ID;
+}
+
+
+int ConditionalFloorIsVirtual(StrBuf *Target, WCTemplputParams *TP)
+{
+	Floor *MyFloor = (Floor *)CTX;
+
+	return MyFloor->ID == VIRTUAL_MY_FLOOR;
+}
+
+
+
+/*******************************************************************************
+ ********************** ROOM Tokens ********************************************
+ ******************************************************************************/
+/**** Name ******/
+void tmplput_RoomName(StrBuf *Target, WCTemplputParams *TP)
+{
+	StrBufAppendTemplate(Target, TP, WC->CurRoom.name, 0);
+}
+void tmplput_current_room(StrBuf *Target, WCTemplputParams *TP)
+{
+	wcsession *WCC = WC;
+
+	if (WCC != NULL)
+		StrBufAppendTemplate(Target, TP, 
+				     WCC->CurRoom.name, 
+				     0); 
+}
+void tmplput_ROOM_NAME(StrBuf *Target, WCTemplputParams *TP) 
+{
+	folder *Folder = (folder *)CTX;
+
+	if (Folder == NULL)
+	{
+		wcsession *WCC = WC;
+
+		if (WCC == NULL)
+			return;
+		Folder = &WCC->CurRoom;
+	}
+	StrBufAppendTemplate(Target, TP, Folder->name, 0);
+}
+void tmplput_ROOM_BASENAME(StrBuf *Target, WCTemplputParams *TP) 
+{
+	folder *room = (folder *)CTX;
+
+	if (room->nRoomNameParts > 1)
+		StrBufAppendTemplate(Target, TP, 
+				      room->RoomNameParts[room->nRoomNameParts - 1], 0);
+	else 
+		StrBufAppendTemplate(Target, TP, room->name, 0);
+}
+void tmplput_ROOM_LEVEL_N_TIMES(StrBuf *Target, WCTemplputParams *TP) 
+{
+	folder *room = (folder *)CTX;
+	int i;
+        const char *AppendMe;
+        long AppendMeLen;
+
+
+	if (room->nRoomNameParts > 1)
+	{
+		GetTemplateTokenString(Target, TP, 0, &AppendMe, &AppendMeLen);
+		for (i = 0; i < room->nRoomNameParts; i++)
+			StrBufAppendBufPlain(Target, AppendMe, AppendMeLen, 0);
+	}
+}
+int ConditionalRoomIsInbox(StrBuf *Target, WCTemplputParams *TP)
+{
+	folder *Folder = (folder *)CTX;
+	return Folder->is_inbox;
+}
+
+/****** Properties ******/
+int ConditionalThisRoomHas_QRFlag(StrBuf *Target, WCTemplputParams *TP)
+{
+	long QR_CheckFlag;
+	wcsession *WCC = WC;
+	
+	QR_CheckFlag = GetTemplateTokenNumber(Target, TP, 2, 0);
+	if (QR_CheckFlag == 0)
+		LogTemplateError(Target, "Conditional", ERR_PARM1, TP,
+				 "requires one of the #\"QR*\"- defines or an integer flag 0 is invalid!");
+	
+	if (WCC == NULL)
+		return 0;
+
+	if ((TP->Tokens->Params[2]->MaskBy == eOR) ||
+	    (TP->Tokens->Params[2]->MaskBy == eNO))
+		return (WCC->CurRoom.QRFlags & QR_CheckFlag) != 0;
+	else
+		return (WCC->CurRoom.QRFlags & QR_CheckFlag) == QR_CheckFlag;
+}
+int ConditionalRoomHas_QRFlag(StrBuf *Target, WCTemplputParams *TP)
+{
+	long QR_CheckFlag;
+	folder *Folder = (folder *)(TP->Context);
+
+	QR_CheckFlag = GetTemplateTokenNumber(Target, TP, 2, 0);
+	if (QR_CheckFlag == 0)
+		LogTemplateError(Target, "Conditional", ERR_PARM1, TP,
+				 "requires one of the #\"QR*\"- defines or an integer flag 0 is invalid!");
+
+	if ((TP->Tokens->Params[2]->MaskBy == eOR) ||
+	    (TP->Tokens->Params[2]->MaskBy == eNO))
+		return (Folder->QRFlags & QR_CheckFlag) != 0;
+	else
+		return (Folder->QRFlags & QR_CheckFlag) == QR_CheckFlag;
+}
+void tmplput_ROOM_QRFLAGS(StrBuf *Target, WCTemplputParams *TP) 
+{
+	folder *Folder = (folder *)CTX;
+	StrBufAppendPrintf(Target, "%d", Folder->QRFlags);
+}
+
+
+int ConditionalThisRoomHas_QRFlag2(StrBuf *Target, WCTemplputParams *TP)
+{
+	long QR2_CheckFlag;
+	wcsession *WCC = WC;
+	
+	QR2_CheckFlag = GetTemplateTokenNumber(Target, TP, 2, 0);
+	if (QR2_CheckFlag == 0)
+		LogTemplateError(Target, "Conditional", ERR_PARM1, TP,
+				 "requires one of the #\"QR2*\"- defines or an integer flag 0 is invalid!");
+
+	
+	if (WCC == NULL)
+		return 0;
+
+	if ((TP->Tokens->Params[2]->MaskBy == eOR) ||
+	    (TP->Tokens->Params[2]->MaskBy == eNO))
+		return (WCC->CurRoom.QRFlags2 & QR2_CheckFlag) != 0;
+	else
+		return (WCC->CurRoom.QRFlags2 & QR2_CheckFlag) == QR2_CheckFlag;
+}
+int ConditionalRoomHas_QRFlag2(StrBuf *Target, WCTemplputParams *TP)
+{
+	long QR2_CheckFlag;
+	folder *Folder = (folder *)(TP->Context);
+
+	QR2_CheckFlag = GetTemplateTokenNumber(Target, TP, 2, 0);
+	if (QR2_CheckFlag == 0)
+		LogTemplateError(Target, "Conditional", ERR_PARM1, TP,
+				 "requires one of the #\"QR2*\"- defines or an integer flag 0 is invalid!");
+	return ((Folder->QRFlags2 & QR2_CheckFlag) != 0);
+}
+
+int ConditionalRoomHas_UAFlag(StrBuf *Target, WCTemplputParams *TP)
+{
+	folder *Folder = (folder *)(TP->Context);
+	long UA_CheckFlag;
+		
+	UA_CheckFlag = GetTemplateTokenNumber(Target, TP, 2, 0);
+	if (UA_CheckFlag == 0)
+		LogTemplateError(Target, "Conditional", ERR_PARM1, TP,
+				 "requires one of the #\"UA_*\"- defines or an integer flag 0 is invalid!");
+
+	return ((Folder->RAFlags & UA_CheckFlag) != 0);
+}
+
+
+
+
+
+void tmplput_ROOM_ACL(StrBuf *Target, WCTemplputParams *TP) 
+{
+	folder *Folder = (folder *)CTX;
+
+	StrBufAppendPrintf(Target, "%ld", Folder->RAFlags, 0);
+}
+
+
+
+void tmplput_ROOM_RAFLAGS(StrBuf *Target, WCTemplputParams *TP) 
+{
+	folder *Folder = (folder *)(TP->Context);
+	StrBufAppendPrintf(Target, "%d", Folder->RAFlags);
+}
+
+
+void tmplput_ThisRoomAide(StrBuf *Target, WCTemplputParams *TP) 
+{
+	wcsession *WCC = WC;
+
+	LoadRoomAide();
+
+	StrBufAppendTemplate(Target, TP, WCC->CurRoom.RoomAide, 0);
+}
+int ConditionalRoomAide(StrBuf *Target, WCTemplputParams *TP)
+{
+	wcsession *WCC = WC;
+	return (WCC != NULL)? 
+		((WCC->CurRoom.RAFlags & UA_ADMINALLOWED) != 0) : 0;
+}
+
+int ConditionalRoomAcessDelete(StrBuf *Target, WCTemplputParams *TP)
+{
+	wcsession *WCC = WC;
+	return (WCC == NULL)? 0 : 
+		( ((WCC->CurRoom.RAFlags & UA_ADMINALLOWED) != 0) ||
+		   (WCC->CurRoom.is_inbox) || 
+		   (WCC->CurRoom.QRFlags2 & QR2_COLLABDEL) );
+}
+
+
+
+int ConditionalHaveRoomeditRights(StrBuf *Target, WCTemplputParams *TP)
+{
+	wcsession *WCC = WC;
+
+	return ( (WCC!= NULL) && 
+		 ((WCC->axlevel >= 6) || 
+		  ((WCC->CurRoom.RAFlags & UA_ADMINALLOWED) != 0) ||
+		  (WCC->CurRoom.is_inbox) ));
+}
+void tmplput_ThisRoomPass(StrBuf *Target, WCTemplputParams *TP) 
+{
+	wcsession *WCC = WC;
+
+	LoadRoomXA();
+
+	StrBufAppendTemplate(Target, TP, WCC->CurRoom.XAPass, 0);
+}
+
+
+
+
+
+
+void tmplput_ThisRoom_nNewMessages(StrBuf *Target, WCTemplputParams *TP) 
+{
+	wcsession *WCC = WC;
+
+	LoadRoomXA();
+
+	StrBufAppendPrintf(Target, "%d", WCC->CurRoom.nNewMessages);
+}
+
+void tmplput_ThisRoom_nTotalMessages(StrBuf *Target, WCTemplputParams *TP) 
+{
+	wcsession *WCC = WC;
+
+	LoadRoomXA();
+
+	StrBufAppendPrintf(Target, "%d", WCC->CurRoom.nTotalMessages);
+}
+
+
+
+
+
+
+void tmplput_ThisRoomOrder(StrBuf *Target, WCTemplputParams *TP) 
+{
+	wcsession *WCC = WC;
+
+	LoadRoomXA();
+
+	StrBufAppendPrintf(Target, "%d", WCC->CurRoom.Order);
+}
+int ConditionalThisRoomOrder(StrBuf *Target, WCTemplputParams *TP)
+{
+	wcsession *WCC = WC;
+	long CheckThis;
+
+	if (WCC == NULL)
+		return 0;
+
+	LoadRoomXA();
+
+	CheckThis = GetTemplateTokenNumber(Target, TP, 2, 0);
+	return CheckThis == WCC->CurRoom.Order;
+}
+void tmplput_ROOM_LISTORDER(StrBuf *Target, WCTemplputParams *TP) 
+{
+	folder *Folder = (folder *)CTX;
+	StrBufAppendPrintf(Target, "%d", Folder->Order);
+}
+
+
+
+
+
+int ConditionalThisRoomXHavePic(StrBuf *Target, WCTemplputParams *TP)
+{
+	wcsession *WCC = WC;
+	
+	if (WCC == NULL)
+		return 0;
+
+	LoadXRoomPic();
+	return WCC->CurRoom.XHaveRoomPic == 1;
+}
+
+
+int ConditionalThisRoomXHaveInfoText(StrBuf *Target, WCTemplputParams *TP)
+{
+	wcsession *WCC = WC;
+	
+	if (WCC == NULL)
+		return 0;
+
+	LoadXRoomInfoText();
+	return (StrLength(WCC->CurRoom.XInfoText)>0);
+}
+void tmplput_ThisRoomInfoText(StrBuf *Target, WCTemplputParams *TP) 
+{
+	wcsession *WCC = WC;
+
+	LoadXRoomInfoText();
+
+	StrBufAppendTemplate(Target, TP, WCC->CurRoom.XAPass, 1);
+}
+
+
+
+
+
+
+
+
+void tmplput_ROOM_LASTCHANGE(StrBuf *Target, WCTemplputParams *TP) 
+{
+	folder *Folder = (folder *)CTX;
+	StrBufAppendPrintf(Target, "%d", Folder->lastchange);
+}
+
+
+
+
+
+
+
+
+void tmplput_ThisRoomDirectory(StrBuf *Target, WCTemplputParams *TP) 
+{
+	wcsession *WCC = WC;
+
+	LoadRoomXA();
+
+	StrBufAppendTemplate(Target, TP, WCC->CurRoom.Directory, 0);
+}
+void tmplput_ThisRoomXNFiles(StrBuf *Target, WCTemplputParams *TP) 
+{
+	wcsession *WCC = WC;
+
+	LoadXRoomXCountFiles();
+
+	StrBufAppendPrintf(Target, "%d", WCC->CurRoom.XDownloadCount);
+}
+
+void tmplput_ThisRoomX_FileString(StrBuf *Target, WCTemplputParams *TP) 
+{
+	wcsession *WCC = WC;
+
+	LoadXRoomXCountFiles();
+
+	if (WCC->CurRoom.XDownloadCount == 1)
+		StrBufAppendBufPlain(Target, _("file"), -1, 0);
+	else
+		StrBufAppendBufPlain(Target, _("files"), -1, 0);
+}
+
+
+void 
+InitModule_ROOMTOKENS
+(void)
+{
+	RegisterNamespace("ROOMBANNER", 0, 1, tmplput_roombanner, NULL, CTX_NONE);
+
+	RegisterNamespace("FLOOR:ID", 0, 0, tmplput_FLOOR_ID, NULL, CTX_FLOORS);
+	RegisterNamespace("ROOM:INFO:FLOORID", 0, 1, tmplput_ROOM_FLOORID, NULL, CTX_ROOMS);
+	RegisterNamespace("ROOM:INFO:FLOOR:ID", 0, 0, tmplput_ROOM_FLOOR_ID, NULL, CTX_ROOMS);
+
+	RegisterNamespace("FLOOR:NAME", 0, 1, tmplput_FLOOR_NAME, NULL, CTX_FLOORS);
+	RegisterNamespace("ROOM:INFO:FLOOR:NAME", 0, 1, tmplput_ROOM_FLOOR_NAME, NULL, CTX_ROOMS);
+	RegisterNamespace("THISROOM:FLOOR:NAME", 0, 1, tmplput_ThisRoomFloorName, NULL, CTX_NONE);
+
+	RegisterNamespace("FLOOR:NROOMS", 0, 0, tmplput_FLOOR_NROOMS, NULL, CTX_FLOORS);
+	RegisterNamespace("ROOM:INFO:FLOOR:NROOMS", 0, 0, tmplput_ROOM_FLOOR_NROOMS, NULL, CTX_ROOMS);
+
+	RegisterConditional(HKEY("COND:FLOOR:ISSUBROOM"), 0, ConditionalFloorIsSUBROOM, CTX_FLOORS);
+	RegisterConditional(HKEY("COND:FLOOR:NROOMS"), 1, ConditionalFloorHaveNRooms, CTX_FLOORS);
+	RegisterConditional(HKEY("COND:ROOM:REST:ISSUBFLOOR"), 0, ConditionalFloorIsRESTSubFloor, CTX_FLOORS);
+	RegisterConditional(HKEY("COND:FLOOR:ISVIRTUAL"), 0, ConditionalFloorIsVirtual, CTX_FLOORS);
+
+	/**** Room... ******/
+        /**** Name ******/
+	RegisterNamespace("ROOMNAME", 0, 1, tmplput_RoomName, NULL, CTX_NONE);
+	RegisterNamespace("CURRENT_ROOM", 0, 1, tmplput_current_room, NULL, CTX_NONE);
+	RegisterNamespace("ROOM:INFO:NAME", 0, 1, tmplput_ROOM_NAME, NULL, CTX_ROOMS);
+	RegisterNamespace("ROOM:INFO:PRINT_NAME", 0, 1, tmplput_ROOM_NAME, NULL, CTX_NONE);
+	RegisterNamespace("ROOM:INFO:BASENAME", 0, 1, tmplput_ROOM_BASENAME, NULL, CTX_ROOMS);
+	RegisterNamespace("ROOM:INFO:LEVELNTIMES", 1, 2, tmplput_ROOM_LEVEL_N_TIMES, NULL, CTX_ROOMS);
+	RegisterConditional(HKEY("COND:ROOM:INFO:IS_INBOX"), 0, ConditionalRoomIsInbox, CTX_ROOMS);
+
+	/****** Properties ******/
+	RegisterNamespace("ROOM:INFO:QRFLAGS", 0, 1, tmplput_ROOM_QRFLAGS, NULL, CTX_ROOMS);
+	RegisterConditional(HKEY("COND:THISROOM:FLAG:QR"), 0, ConditionalThisRoomHas_QRFlag, CTX_NONE);
+	RegisterConditional(HKEY("COND:ROOM:FLAG:QR"), 0, ConditionalRoomHas_QRFlag, CTX_ROOMS);
+
+	RegisterConditional(HKEY("COND:THISROOM:FLAG:QR2"), 0, ConditionalThisRoomHas_QRFlag2, CTX_NONE);
+	RegisterConditional(HKEY("COND:ROOM:FLAG:QR2"), 0, ConditionalRoomHas_QRFlag2, CTX_ROOMS);
+
+	RegisterConditional(HKEY("COND:ROOM:FLAG:UA"), 0, ConditionalRoomHas_UAFlag, CTX_ROOMS);
+	RegisterNamespace("ROOM:INFO:RAFLAGS", 0, 1, tmplput_ROOM_RAFLAGS, NULL, CTX_ROOMS);
+
+
+	RegisterNamespace("ROOM:INFO:LISTORDER", 0, 1, tmplput_ROOM_LISTORDER, NULL, CTX_ROOMS);
+	RegisterNamespace("THISROOM:ORDER", 0, 0, tmplput_ThisRoomOrder, NULL, CTX_NONE);
+	RegisterConditional(HKEY("COND:THISROOM:ORDER"), 0, ConditionalThisRoomOrder, CTX_NONE);
+
+	RegisterNamespace("ROOM:INFO:LASTCHANGE", 0, 1, tmplput_ROOM_LASTCHANGE, NULL, CTX_ROOMS);
+
+	RegisterNamespace("THISROOM:MSGS:NEW", 0, 0, tmplput_ThisRoom_nNewMessages, NULL, CTX_NONE);
+	RegisterNamespace("THISROOM:MSGS:TOTAL", 0, 0, tmplput_ThisRoom_nTotalMessages, NULL, CTX_NONE);
+
+	RegisterNamespace("THISROOM:PASS", 0, 1, tmplput_ThisRoomPass, NULL, CTX_NONE);
+	RegisterNamespace("THISROOM:AIDE", 0, 1, tmplput_ThisRoomAide, NULL, CTX_NONE);
+	RegisterConditional(HKEY("COND:ROOMAIDE"), 2, ConditionalRoomAide, CTX_NONE);
+	RegisterConditional(HKEY("COND:ACCESS:DELETE"), 2, ConditionalRoomAcessDelete, CTX_NONE);
+	RegisterConditional(HKEY("COND:ROOM:EDITACCESS"), 0, ConditionalHaveRoomeditRights, CTX_NONE);
+
+	RegisterConditional(HKEY("COND:THISROOM:HAVE_PIC"), 0, ConditionalThisRoomXHavePic, CTX_NONE);
+
+	RegisterNamespace("THISROOM:INFOTEXT", 1, 2, tmplput_ThisRoomInfoText, NULL, CTX_NONE);
+	RegisterConditional(HKEY("COND:THISROOM:HAVE_INFOTEXT"), 0, ConditionalThisRoomXHaveInfoText, CTX_NONE);
+
+	RegisterNamespace("THISROOM:FILES:N", 0, 1, tmplput_ThisRoomXNFiles, NULL, CTX_NONE);
+	RegisterNamespace("THISROOM:FILES:STR", 0, 1, tmplput_ThisRoomX_FileString, NULL, CTX_NONE);
+	RegisterNamespace("THISROOM:DIRECTORY", 0, 1, tmplput_ThisRoomDirectory, NULL, CTX_NONE);
+
+
+
+
+
+
+	RegisterNamespace("ROOM:INFO:ACL", 0, 1, tmplput_ROOM_ACL, NULL, CTX_ROOMS);
+}
---
+++ webcit/roomviews.c
@@ -0,0 +1,267 @@
+/*
+ * $Id$
+ * Lots of different room-related operations.
+ */
+
+#include "webcit.h"
+#include "webserver.h"
+
+char *viewdefs[VIEW_MAX];			/* the different kinds of available views */
+
+ROOM_VIEWS exchangeable_views[VIEW_MAX][VIEW_MAX] = {	/* the different kinds of available views for a view */
+{VIEW_BBS, VIEW_MAILBOX, VIEW_MAX, VIEW_MAX, VIEW_MAX, VIEW_MAX, VIEW_MAX, VIEW_MAX, VIEW_MAX, VIEW_MAX }, 
+{VIEW_BBS, VIEW_MAILBOX, VIEW_MAX, VIEW_MAX, VIEW_MAX, VIEW_MAX, VIEW_MAX, VIEW_MAX, VIEW_MAX, VIEW_MAX }, 
+{VIEW_MAX, VIEW_MAX, VIEW_ADDRESSBOOK, VIEW_CALENDAR, VIEW_MAX, VIEW_MAX, VIEW_MAX, VIEW_MAX, VIEW_MAX, VIEW_MAX }, 
+{VIEW_MAX, VIEW_MAX, VIEW_MAX, VIEW_CALENDAR, VIEW_MAX, VIEW_MAX, VIEW_MAX, VIEW_MAX /*VIEW_CALBRIEF*/, VIEW_MAX, VIEW_MAX }, 
+{VIEW_MAX, VIEW_MAX, VIEW_MAX, VIEW_MAX, VIEW_TASKS, VIEW_MAX, VIEW_MAX, VIEW_MAX, VIEW_MAX, VIEW_MAX, },
+{VIEW_MAX, VIEW_MAX, VIEW_MAX, VIEW_MAX, VIEW_MAX, VIEW_NOTES, VIEW_MAX, VIEW_MAX, VIEW_MAX, VIEW_MAX, },
+{VIEW_MAX, VIEW_MAX, VIEW_MAX, VIEW_MAX, VIEW_MAX, VIEW_MAX, VIEW_WIKI, VIEW_MAX, VIEW_MAX, VIEW_MAX}, 
+{VIEW_MAX, VIEW_MAX, VIEW_MAX, VIEW_CALENDAR, VIEW_MAX, VIEW_MAX, VIEW_MAX, VIEW_MAX/*VIEW_CALBRIEF*/, VIEW_MAX, VIEW_MAX},
+{VIEW_MAX, VIEW_MAX, VIEW_MAX, VIEW_MAX, VIEW_MAX, VIEW_MAX, VIEW_MAX, VIEW_MAX, VIEW_JOURNAL, VIEW_MAX }, 
+{VIEW_MAX, VIEW_MAX, VIEW_MAX, VIEW_MAX, VIEW_MAX, VIEW_MAX, VIEW_MAX, VIEW_MAX, VIEW_MAX, VIEW_BLOG }, 
+	};
+/* the brief calendar view is disabled: VIEW_CALBRIEF */
+
+ROOM_VIEWS allowed_default_views[VIEW_MAX] = {
+	1, /* VIEW_BBS		Bulletin board view */
+	1, /* VIEW_MAILBOX		Mailbox summary */
+	1, /* VIEW_ADDRESSBOOK	Address book view */
+	1, /* VIEW_CALENDAR		Calendar view */
+	1, /* VIEW_TASKS		Tasks view */
+	1, /* VIEW_NOTES		Notes view */
+	1, /* VIEW_WIKI		Wiki view */
+	0, /* VIEW_CALBRIEF		Brief Calendar view */
+	0, /* VIEW_JOURNAL		Journal view */
+	0  /* VIEW_BLOG		Blog view (not yet implemented) */
+};
+
+
+/*
+ * Initialize the viewdefs with localized strings
+ */
+void initialize_viewdefs(void) {
+	viewdefs[VIEW_BBS] = _("Bulletin Board");
+	viewdefs[VIEW_MAILBOX] = _("Mail Folder");
+	viewdefs[VIEW_ADDRESSBOOK] = _("Address Book");
+	viewdefs[VIEW_CALENDAR] = _("Calendar");
+	viewdefs[VIEW_TASKS] = _("Task List");
+	viewdefs[VIEW_NOTES] = _("Notes List");
+	viewdefs[VIEW_WIKI] = _("Wiki");
+	viewdefs[VIEW_CALBRIEF] = _("Calendar List");
+	viewdefs[VIEW_JOURNAL] = _("Journal");
+	viewdefs[VIEW_BLOG] = _("Blog");
+}
+
+
+
+void tmplput_ROOM_COLLECTIONTYPE(StrBuf *Target, WCTemplputParams *TP) 
+{
+	folder *Folder = (folder *)CTX;
+	
+	switch(Folder->view) {
+	case VIEW_CALENDAR:
+		StrBufAppendBufPlain(Target, HKEY("vevent"), 0);
+		break;
+	case VIEW_TASKS:
+		StrBufAppendBufPlain(Target, HKEY("vtodo"), 0);
+		break;
+	case VIEW_ADDRESSBOOK:
+		StrBufAppendBufPlain(Target, HKEY("vcard"), 0);
+		break;
+	case VIEW_NOTES:
+		StrBufAppendBufPlain(Target, HKEY("vnotes"), 0);
+		break;
+	case VIEW_JOURNAL:
+		StrBufAppendBufPlain(Target, HKEY("vjournal"), 0);
+		break;
+	case VIEW_WIKI:
+		StrBufAppendBufPlain(Target, HKEY("wiki"), 0);
+		break;
+	}
+}
+
+
+
+int ConditionalRoomHasGroupdavContent(StrBuf *Target, WCTemplputParams *TP)
+{
+	folder *Folder = (folder *)CTX;
+
+	lprintf(0, "-> %s: %ld\n", ChrPtr(Folder->name), Folder->view);
+
+	return ((Folder->view == VIEW_CALENDAR) || 
+		(Folder->view == VIEW_TASKS) || 
+		(Folder->view == VIEW_ADDRESSBOOK) ||
+		(Folder->view == VIEW_NOTES) ||
+		(Folder->view == VIEW_JOURNAL) );
+}
+
+
+
+
+int ConditionalIsRoomtype(StrBuf *Target, WCTemplputParams *TP)
+{
+	wcsession *WCC = WC;
+
+	if ((WCC == NULL) ||
+	    (TP->Tokens->nParameters < 3))
+	{
+		return ((WCC->CurRoom.view < VIEW_BBS) || 
+			(WCC->CurRoom.view > VIEW_MAX));
+	}
+
+	return WCC->CurRoom.view == GetTemplateTokenNumber(Target, TP, 2, VIEW_BBS);
+}
+
+
+void tmplput_CurrentRoomViewString(StrBuf *Target, WCTemplputParams *TP) 
+{
+	wcsession *WCC = WC;
+	StrBuf *Buf;
+
+	if ((WCC == NULL) ||
+	    (WCC->CurRoom.defview >= VIEW_MAX) || 
+	    (WCC->CurRoom.defview < VIEW_BBS))
+	{
+		LogTemplateError(Target, "Token", ERR_PARM2, TP,
+				 "Roomview [%ld] not valid\n", 
+				 (WCC != NULL)? 
+				 WCC->CurRoom.defview : -1);
+		return;
+	}
+
+	Buf = NewStrBufPlain(_(viewdefs[WCC->CurRoom.defview]), -1);
+	StrBufAppendTemplate(Target, TP, Buf, 0);
+	FreeStrBuf(&Buf);
+}
+
+void tmplput_RoomViewString(StrBuf *Target, WCTemplputParams *TP) 
+{
+	long CheckThis;
+	StrBuf *Buf;
+
+	CheckThis = GetTemplateTokenNumber(Target, TP, 0, 0);
+	if ((CheckThis >= VIEW_MAX) || (CheckThis < VIEW_BBS))
+	{
+		LogTemplateError(Target, "Token", ERR_PARM2, TP,
+				 "Roomview [%ld] not valid\n", 
+				 CheckThis);
+		return;
+	}
+
+	Buf = NewStrBufPlain(_(viewdefs[CheckThis]), -1);
+	StrBufAppendTemplate(Target, TP, Buf, 0);
+	FreeStrBuf(&Buf);
+}
+
+
+
+int ConditionalIsAllowedDefaultView(StrBuf *Target, WCTemplputParams *TP)
+{
+	wcsession *WCC = WC;
+	long CheckThis;
+	
+	if (WCC == NULL)
+		return 0;
+
+	CheckThis = GetTemplateTokenNumber(Target, TP, 2, 0);
+	if ((CheckThis >= VIEW_MAX) || (CheckThis < VIEW_BBS))
+	{
+		LogTemplateError(Target, "Conditional", ERR_PARM2, TP,
+				 "Roomview [%ld] not valid\n", 
+				 CheckThis);
+		return 0;
+	}
+
+	return allowed_default_views[CheckThis] != 0;
+}
+
+int ConditionalThisRoomDefView(StrBuf *Target, WCTemplputParams *TP)
+{
+	wcsession *WCC = WC;
+	long CheckThis;
+
+	if (WCC == NULL)
+		return 0;
+
+	CheckThis = GetTemplateTokenNumber(Target, TP, 2, 0);
+	return CheckThis == WCC->CurRoom.defview;
+}
+
+int ConditionalThisRoomCurrView(StrBuf *Target, WCTemplputParams *TP)
+{
+	wcsession *WCC = WC;
+	long CheckThis;
+
+	if (WCC == NULL)
+		return 0;
+
+	CheckThis = GetTemplateTokenNumber(Target, TP, 2, 0);
+	return CheckThis == WCC->CurRoom.view;
+}
+
+int ConditionalThisRoomHaveView(StrBuf *Target, WCTemplputParams *TP)
+{
+	wcsession *WCC = WC;
+	long CheckThis;
+	
+	if (WCC == NULL)
+		return 0;
+
+	CheckThis = GetTemplateTokenNumber(Target, TP, 2, 0);
+	if ((CheckThis >= VIEW_MAX) || (CheckThis < VIEW_BBS))
+	{
+		LogTemplateError(Target, "Conditional", ERR_PARM2, TP,
+				 "Roomview [%ld] not valid\n", 
+				 CheckThis);
+		return 0;
+	}
+
+	return exchangeable_views [WCC->CurRoom.defview][CheckThis] != VIEW_MAX;
+}
+
+void tmplput_ROOM_VIEW(StrBuf *Target, WCTemplputParams *TP) 
+{
+	folder *Folder = (folder *)CTX;
+	StrBufAppendPrintf(Target, "%d", Folder->view);
+}
+void tmplput_ROOM_DEFVIEW(StrBuf *Target, WCTemplputParams *TP) 
+{
+	folder *Folder = (folder *)CTX;
+	StrBufAppendPrintf(Target, "%d", Folder->defview);
+}
+
+
+void tmplput_CurrentRoomDefView(StrBuf *Target, WCTemplputParams *TP) 
+{
+	wcsession *WCC = WC;
+
+	StrBufAppendPrintf(Target, "%d", WCC->CurRoom.defview);
+}
+
+void 
+InitModule_ROOMVIEWS
+(void)
+{
+	initialize_viewdefs();
+
+	RegisterNamespace("THISROOM:VIEW_STRING", 0, 1, tmplput_CurrentRoomViewString, NULL, CTX_NONE);
+	RegisterNamespace("ROOM:VIEW_STRING", 1, 2, tmplput_RoomViewString, NULL, CTX_NONE);
+
+	RegisterConditional(HKEY("COND:ALLOWED_DEFAULT_VIEW"), 0, ConditionalIsAllowedDefaultView, CTX_NONE);
+	RegisterConditional(HKEY("COND:THISROOM:DEFAULT_VIEW"), 0, ConditionalThisRoomDefView, CTX_NONE);
+	RegisterNamespace("THISROOM:DEFAULT_VIEW", 0, 0, tmplput_CurrentRoomDefView, NULL, CTX_NONE);
+	RegisterNamespace("ROOM:INFO:DEFVIEW", 0, 1, tmplput_ROOM_DEFVIEW, NULL, CTX_ROOMS);
+
+	RegisterConditional(HKEY("COND:ROOM:TYPE_IS"), 0, ConditionalIsRoomtype, CTX_NONE);
+
+	RegisterConditional(HKEY("COND:THISROOM:HAVE_VIEW"), 0, ConditionalThisRoomHaveView, CTX_NONE);
+	RegisterConditional(HKEY("COND:ROOM:GROUPDAV_CONTENT"), 0, ConditionalRoomHasGroupdavContent, CTX_ROOMS);
+
+	RegisterConditional(HKEY("COND:THISROOM:CURR_VIEW"), 0, ConditionalThisRoomCurrView, CTX_NONE);
+	RegisterNamespace("ROOM:INFO:VIEW", 0, 1, tmplput_ROOM_VIEW, NULL, CTX_ROOMS);
+
+	RegisterNamespace("ROOM:INFO:COLLECTIONTYPE", 0, 1, tmplput_ROOM_COLLECTIONTYPE, NULL, CTX_ROOMS);
+
+
+
+}
