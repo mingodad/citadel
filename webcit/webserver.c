a74a9bb0d8877ff5610ce24e6402b405bcf949f0
Art Cancro <ajc@citadel.org>
2010-09-14 22:35:29 -0400

    Revert "No more robots.txt, we now welcome spiders"
    This reverts commit 593d0c23d2422a6a1fc9ceb7de9bda66335e0836.

--- webcit/webserver.c
+++ webcit/webserver.c
@@ -4,20 +4,8 @@
  * connection is established, it calls context_loop() from context_loop.c.
  *
  * Copyright (c) 1996-2010 by the citadel.org developers.
+ * This program is released under the terms of the GNU General Public License v3.
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
 #include "webcit.h"
