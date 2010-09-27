28a470b26419c29e20eb9b1e24acbdeb542bca75
Wilfried Goesgens <dothebart@citadel.org>
2010-09-27 20:54:53 +0200

    * get .po infrastructure in place

---
+++ citadel/po/create-pot.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+echo Updating citadel.pot from strings in the source code ...
+xgettext \
+	--copyright-holder='The Citadel Project - http://www.citadel.org' \
+        --from-code='utf-8' \
+	-k_ \
+	-o citadel-setup.pot \
+	--add-comments \
+        ../utils/setup.c 
+
+#xgettext \
+#	--copyright-holder='The Citadel Project - http://www.citadel.org' \
+#        --from-code='utf-8' \
+#	-k_ \
+#	-o citadel-server.pot \
+#	--add-comments \
+#	../*.c \
+#        `cat ../Make_sources | sed  -e "s;.*+= ;;" ` 
+#
+#
+#xgettext \
+#	--copyright-holder='The Citadel Project - http://www.citadel.org' \
+#        --from-code='utf-8' \
+#	-k_ \
+#	-o citadel-client.pot \
+#	--add-comments \
+#	../*.c \
+#        ../textclient/*.c
+
+for x in *.po
+do
+	echo Merging webcit.pot into $x ...
+	msgmerge $x webcit.pot -o $x
+done
