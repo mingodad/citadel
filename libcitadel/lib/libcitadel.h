c973c74dd07b010e70477d31ca325a8d7ba28190
Art Cancro <ajc@citadel.org>
2010-12-20 22:23:49 -0500

    update version number in preparation for 7.85 release

--- libcitadel/lib/libcitadel.h
+++ libcitadel/lib/libcitadel.h
@@ -10,7 +10,7 @@
 #include <time.h>
 #include <stdlib.h>
 #include <stdarg.h>
-#define LIBCITADEL_VERSION_NUMBER	784
+#define LIBCITADEL_VERSION_NUMBER	785
 
 /*
  * Here's a bunch of stupid magic to make the MIME parser portable.
