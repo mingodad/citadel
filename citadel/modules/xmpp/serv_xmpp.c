210668a5518b341d898e79f09df78c6aeaaa01dd
Art Cancro <ajc@citadel.org>
2011-02-18 11:20:52 -0500

    Enable XMPP over TLS

--- citadel/modules/xmpp/serv_xmpp.c
+++ citadel/modules/xmpp/serv_xmpp.c
@@ -144,7 +144,7 @@ void xmpp_stream_start(void *data, const char *supplied_el, const char **attr)
 	/* The features of this stream are... */
 	cprintf("<stream:features>");
 
-#ifdef HAVE_OPENSSL_XXXX_COMMENTED_OUT
+#ifdef HAVE_OPENSSL
 	/* TLS encryption (but only if it isn't already active) */
 	if (!CC->redirect_ssl) {
 		cprintf("<starttls xmlns='urn:ietf:params:xml:ns:xmpp-tls'></starttls>");
