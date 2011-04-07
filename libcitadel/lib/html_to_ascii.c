bca06b89514d8d91c1442735272ab10ea0e19f9a
Art Cancro <ajc@uncensored.citadel.org>
2011-04-06 23:17:23 -0400

    Added GPL declaration to source files in libcitadel

--- libcitadel/lib/html_to_ascii.c
+++ libcitadel/lib/html_to_ascii.c
@@ -2,7 +2,7 @@
  * Functions which handle translation between HTML and plain text
  * Copyright (c) 2000-2010 by the citadel.org team
  *
- * This program is free software; you can redistribute it and/or modify
+ * This program is open source software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
