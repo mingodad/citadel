c04f07529f71e61bd113efb5f4edc2752d73e805
root <root@spiegel.wunderland.vpn42>
2011-05-15 00:49:28 +0200

    Added new code for a slideshow taken from http://snippets.dzone.com/posts/show/1068

--- webcit/static/t/files/picview.js
+++ webcit/static/t/files/picview.js
@@ -1,19 +1,33 @@
 <??("COND:BSTR", 1, "frame")>
 <?=("head")>
 <?=("important_msg")><?%("COND:LOGGEDIN", 1, 1, 1, "", ="paging")>
+
 <div id="banner">
-<h1>
-<?_("Pictures in")><?ROOM:NAME>
-</h1>
+	<h1><?_("Pictures in")><?ROOM:NAME></h1>
 </div>
 <div id="content" class="service">
-<table class="downloads_background"><tr><td>
-<script type="text/javascript" language="JavaScript" > nvar fadeimages=new Array()
-<?ITERATE("ROOM:FILES", ="files_section_onefile_picview")>
-</script>
-<tr><td><script type="text/javascript" src="static/fadeshow.js">
-</script>
-<script type="text/javascript" >
-new fadeshow(fadeimages, 500, 400, 0, 3000, 1, "R");
-</script></td><th>
+	<table class="downloads_background">
+	<tr><td>
+		<script type="text/javascript" language="JavaScript">
+			<?ITERATE("ROOM:FILES", ="files_section_onefile_picview")>
+		</script>
+	</td></tr>
+	<tr><td>
+		<script type="text/javascript">
+			start_slideshow(1, $lastfile, 3000);
+    
+    			function start_slideshow(start_frame, end_frame, delay) {
+        			setTimeout(switch_slides(start_frame,start_frame,end_frame, delay), delay);
+    			}
+                            
+    			function switch_slides(frame, start_frame, end_frame, delay) {
+        			return (function() {
+            				Effect.Fade('slideshow' + frame);
+		            		if (frame == end_frame) { frame = start_frame; } else { frame = frame + 1; }
+            					setTimeout("Effect.Appear('slideshow" + frame + "');", 850);
+            					setTimeout(switch_slides(frame, start_frame, end_frame, delay), delay + 850);
+        			})
+    			}
+		</script>
+	</td><th>
 <?=("trailing")>
