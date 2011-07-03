53d9b46861650835b0994566a728c4ba3da8413a
Wilfried Goesgens <dothebart@citadel.org>
2011-07-03 13:47:55 +0000

    Add ability to hex encode binary strings
     - when encoding plain md5 binary buffers we have to pass the length into the hex encoder, add parameter
     - add wrapper providing old schematic
     - fix documentation of StrBufSipLine()

--- libcitadel/lib/libcitadel.h
+++ libcitadel/lib/libcitadel.h
@@ -287,6 +287,7 @@ int StrBufSanitizeAscii(StrBuf *Buf, const char Mute);
 #define QU			(3)
 void StrBufUrlescAppend(StrBuf *OutBuf, const StrBuf *In, const char *PlainIn);
 void StrBufHexescAppend(StrBuf *OutBuf, const StrBuf *In, const char *PlainIn);
+void StrBufHexEscAppend(StrBuf *OutBuf, const StrBuf *In, const unsigned char *PlainIn, long PlainInLen);
 long StrEscAppend(StrBuf *Target, const StrBuf *Source, const char *PlainIn, int nbsp, int nolinebreaks);
 long StrECMAEscAppend(StrBuf *Target, const StrBuf *Source, const char *PlainIn);
 long StrHtmlEcmaEscAppend(StrBuf *Target, const StrBuf *Source, const char *PlainIn, int nbsp, int nolinebreaks);
