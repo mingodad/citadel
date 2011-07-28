cc1ac05448414ebbdfb65a7f7e1d3081a5618570
Wilfried Goesgens <dothebart@citadel.org>
2011-07-28 12:13:42 +0000

    enhance GetServerStatus to GetServerStatusMsg; supply migration define
    GetServerStatusMsg() can now directly add the server reply text after the status code into WC->ImportantMsg.

--- webcit/tcp_sockets.h
+++ webcit/tcp_sockets.h
@@ -2,7 +2,20 @@ int uds_connectsock(char *);
 int tcp_connectsock(char *, char *);
 int serv_getln(char *strbuf, int bufsize);
 int StrBuf_ServGetln(StrBuf *buf);
-int GetServerStatus(StrBuf *Line, long* FullState);
+
+/**
+ * @brief parse & check the server reply 
+ * @param Line the line containing the server reply
+ * @param FullState if you need more than just the major number, this is returns it. Ignored if NULL.
+ * @param PutImportantMessage if you want to forward the text part of the server reply to the user, specify 1; 
+ *    the result will be put into the 'Important Message' framework.
+ * @param MajorOK in case of which major number not to put the ImportantMessage? 0 for all.
+ * @return the most significant digit of the server status
+ * /
+int GetServerStatusMsg(StrBuf *Line, long* FullState, int PutImportantMessage, int MajorOK);
+/* to migrate old calls.... */
+#define GetServerStatus(a, b) GetServerStatusMsg(a, b, 0, 0)
+
 int serv_puts(const char *string);
 
 int serv_write(const char *buf, int nbytes);
