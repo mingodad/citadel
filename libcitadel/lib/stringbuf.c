f560f5c7f1c1f4915d0a8ff80b8bfb1714e3979b
Wilfried Goesgens <dothebart@citadel.org>
2011-08-19 17:17:31 +0000

    add missing var definition *ups*

--- libcitadel/lib/stringbuf.c
+++ libcitadel/lib/stringbuf.c
@@ -1996,6 +1996,7 @@ long StrECMAEscAppend(StrBuf *Target, const StrBuf *Source, const char *PlainIn)
 	const char *aptr, *eiptr;
 	char *bptr, *eptr;
 	long len;
+	int IsUtf8Sequence;
 
 	if (((Source == NULL) && (PlainIn == NULL)) || (Target == NULL) )
 		return -1;
