f712b85f8ccd3f9ada3fac2dda8f5ced66658c2a
Wilfried Goesgens <dothebart@citadel.org>
2011-08-20 09:50:26 +0000

    When QP encoding, we need to convert blanks to underscores
      - Spamassasin test 'BAD_ENC_HEADER' matches if a subject looks like that:
         Subject: =?UTF-8?Q?=5Btestml=5D l=C3=B6sch misch ey.?=
        it needs to look like that:
         Subject: =?UTF-8?Q?=5Btestml=5D_l=C3=B6sch_misch_ey.?=

--- libcitadel/lib/stringbuf.c
+++ libcitadel/lib/stringbuf.c
@@ -2680,6 +2680,7 @@ int StrBufRFC2047encode(StrBuf **target, const StrBuf *source)
 		    (ch  > 126) || 
 		    (ch ==  61) ||
 		    (ch == '=') ||
+		    (ch == '_') ||
 		    (ch == '[') ||
 		    (ch == ']')   )
 		{
@@ -2687,7 +2688,10 @@ int StrBufRFC2047encode(StrBuf **target, const StrBuf *source)
 			(*target)->BufUsed += 3;
 		}
 		else {
-			(*target)->buf[(*target)->BufUsed] = ch;
+			if (ch == ' ')
+				(*target)->buf[(*target)->BufUsed] = '_';
+			else
+				(*target)->buf[(*target)->BufUsed] = ch;
 			(*target)->BufUsed++;
 		}
 	}
