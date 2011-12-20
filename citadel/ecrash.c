b2331c2f4ffdb5d3be7d03b7184ccdd3ea3bc43f
Art Cancro <ajc@uncensored.citadel.org>
2011-12-20 15:30:26 -0500

    Found some more instances of the phrase 'free software' and changed them to 'open source' to piss off Richard Stallman

--- citadel/ecrash.c
+++ citadel/ecrash.c
@@ -11,7 +11,7 @@
  *
  * vim: ts=4
  *
- * This program is free software; you can redistribute it and/or modify
+ * This program is open source software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
