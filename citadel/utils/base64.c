b55730dc4ec7638231d6f56f2f53b178f1ba2c96
Art Cancro <ajc@uncensored.citadel.org>
2012-01-30 14:40:34 -0500

    More changes of 'free software' to 'open source' made for the express purpose of pissing off linguistic communist Richard Stallman

--- citadel/utils/base64.c
+++ citadel/utils/base64.c
@@ -3,19 +3,13 @@
  * Public domain by John Walker, August 11 1997
  * Modified slightly for the Citadel system, June 1999
  *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 3 of the License, or
- *  (at your option) any later version.
+ *  This program is open source software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 3.
  *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
 
