e5f8f6584c172e6f9492ecff056173b08419854c
Art Cancro <ajc@uncensored.citadel.org>
2012-03-28 23:23:17 -0400

    webcit 8.06

--- webcit/webcit.h
+++ webcit/webcit.h
@@ -129,7 +129,7 @@ extern char *ssl_cipher_list;
 #define PORT_NUM		2000		/* port number to listen on */
 #define DEVELOPER_ID		0
 #define CLIENT_ID		4
-#define CLIENT_VERSION		805		/* This version of WebCit */
+#define CLIENT_VERSION		806		/* This version of WebCit */
 #define MINIMUM_CIT_VERSION	805		/* min required Citadel ver */
 #define	LIBCITADEL_MIN		805		/* min required libcitadel ver */
 #define DEFAULT_HOST		"localhost"	/* Default Citadel server */
