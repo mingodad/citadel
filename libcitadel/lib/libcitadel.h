452067d76c1c0a1824321394db4af2c4f1438af0
Art Cancro <ajc@uncensored.citadel.org>
2012-06-26 10:51:02 -0400

    changing internal version number to 8.12

--- libcitadel/lib/libcitadel.h
+++ libcitadel/lib/libcitadel.h
@@ -28,7 +28,7 @@
 #include <sys/types.h>
 #include <netinet/in.h>
 
-#define LIBCITADEL_VERSION_NUMBER	811
+#define LIBCITADEL_VERSION_NUMBER	812
 
 /*
  * Here's a bunch of stupid magic to make the MIME parser portable.
