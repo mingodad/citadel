66e08c8d10b9751ea638f9bbb8b9da739a6ee12b
Art Cancro <ajc@uncensored.citadel.org>
2012-07-13 10:37:00 -0400

    serv_checkpoint needs no db headers

--- citadel/modules/checkpoint/serv_checkpoint.c
+++ citadel/modules/checkpoint/serv_checkpoint.c
@@ -1,21 +1,15 @@
 /*
  * checkpointing module for the database
  *
- * Copyright (c) 1987-2011 by the citadel.org team
+ * Copyright (c) 1987-2012 by the citadel.org team
  *
  * This program is open source software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as published
- * by the Free Software Foundation; either version 3 of the License, or
- * (at your option) any later version.
+ * modify it under the terms of the GNU General Public License, version 3.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
  
 #include "sysdep.h"
@@ -25,29 +19,12 @@
 #include <ctype.h>
 #include <string.h>
 #include <errno.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <dirent.h>
-
-#ifdef HAVE_DB_H
-#include <db.h>
-#elif defined(HAVE_DB4_DB_H)
-#include <db4/db.h>
-#else
-#error Neither <db.h> nor <db4/db.h> was found by configure. Install db4-devel.
-#endif
-
-
-#if DB_VERSION_MAJOR < 4 || DB_VERSION_MINOR < 1
-#error Citadel requires Berkeley DB v4.1 or newer.  Please upgrade.
-#endif
 
 #include <libcitadel.h>
 
 #include "citadel.h"
 #include "server.h"
 #include "citserver.h"
-#include "database.h"
 #include "msgbase.h"
 #include "sysdep_decls.h"
 #include "config.h"
@@ -55,8 +32,6 @@
 
 #include "ctdl_module.h"
 #include "context.h"
- 
-
 
 CTDL_MODULE_INIT(checkpoint) {
 	if (threading)
