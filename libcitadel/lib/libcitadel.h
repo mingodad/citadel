53481be28de6082a8f59dad096ff7df7601fa1a7
Art Cancro <ajc@uncensored.citadel.org>
2012-07-20 09:58:14 -0400

    version number to 8.14 for upcoming release

--- libcitadel/lib/libcitadel.h
+++ libcitadel/lib/libcitadel.h
@@ -28,7 +28,7 @@
 #include <sys/types.h>
 #include <netinet/in.h>
 
-#define LIBCITADEL_VERSION_NUMBER	813
+#define LIBCITADEL_VERSION_NUMBER	814
 
 /*
  * Here's a bunch of stupid magic to make the MIME parser portable.
