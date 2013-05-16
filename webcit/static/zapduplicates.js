e62e0018851d7d4c5ef3480192047bc4f744b110
Wilfried Goesgens <dothebart@citadel.org>
2013-05-17 00:14:19 +0200

    Webcit dupe-zapper
    A simple duplicate message finder - criterias are the subject, sender, sent date. If they're the same, its a duplicate. Messages are then moved to the trashcan of the current user.

---
+++ webcit/static/zapduplicates.js
@@ -0,0 +1,64 @@
+function loadZapMessages(transport)
+{
+    var dupes = '';
+    var dupcount = 0;
+    try {
+	var data = evalJSON(transport.responseText);
+	if (!!data && transport.responseText.length < 2) {
+	    alert("Message loading failed");
+	} 
+	nummsgs = data['nummsgs'];
+	msgs = data['msgs'];
+	var length = msgs.length;
+	rowArray = new Array(length); // store so they can be sorted
+	WCLog("Row array length: "+rowArray.length);
+	for(var x=1; x<length; ++x) {
+	    var currentRow = msgs[x];
+	    var LastRow = msgs[x-1];
+	    if ((currentRow[1] == LastRow[1]) &&
+		(currentRow[2] == LastRow[2]) &&
+		(currentRow[4] == LastRow[4]))
+	    {
+		dupcount ++;
+		dupes += currentRow[0] + ',' ;
+		if (dupes.length > 800) {
+		    var mvCommand = "g_cmd=MOVE%20" + dupes + "|"+encodeURIComponent('Trash')+"|0";
+		    new Ajax.Request("ajax_servcmd", {
+			parameters: mvCommand,
+			method: 'post',
+		    });
+		    dupes = "";
+		}
+
+	    }
+
+	}
+
+    } catch (e) {
+	window.alert(e+"|"+e.description);
+    }
+    var mvCommand = "g_cmd=MOVE%20" + dupes + "|"+encodeURIComponent('Trash')+"|0";
+    new Ajax.Request("ajax_servcmd", {
+	parameters: mvCommand,
+	method: 'post',
+    });
+
+    alert ('deleted: '+dupcount+'messages');
+}
+
+function TriggerLoadDupes ()
+{
+//    alert("bla");
+// http://127.0.0.1:2000/roommsgs?room=test%20rss&startmsg=0&stopmsg=499&SortBy=Subject&SortOrder=1
+	roomName = getTextContent(document.getElementById("rmname"));
+    var parameters = {'room':roomName, 'startmsg': 0, 'stopmsg': -1, 'SortBy' : 'Subject', 'SortOrder' : 1};
+
+	new Ajax.Request("roommsgs", {
+		method: 'get',
+				onSuccess: loadZapMessages,
+				parameters: parameters,
+				sanitize: false,
+				evalJSON: false,
+				onFailure: function(e) { alert("Failure: " + e);}
+	});
+}
