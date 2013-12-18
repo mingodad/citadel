1cc036dfd97eb78144040b27ade8662ea82a56cc
Art Cancro <ajc@uncensored.citadel.org>
2013-12-18 17:45:46 -0500

    push tags

--- libcitadel/lib/libcitadel.h
+++ libcitadel/lib/libcitadel.h
@@ -28,7 +28,7 @@
 #include <sys/types.h>
 #include <netinet/in.h>
 
-#define LIBCITADEL_VERSION_NUMBER	822
+#define LIBCITADEL_VERSION_NUMBER	823
 
 /*
  * Here's a bunch of stupid magic to make the MIME parser portable.
