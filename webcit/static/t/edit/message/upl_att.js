ede2dea3fa3b514ecf044b1f51b9c40f912269b0
Wilfried Goesgens <dothebart@citadel.org>
2014-01-21 21:27:50 +0100

    Attachments: start using fineuploader.com to upload attachments to messages.

---
+++ webcit/static/t/edit/message/upl_att.js
@@ -0,0 +1,9 @@
+{
+<?!("COND:BSTR", 1, "UPLOAD_ERROR")>
+"Error" : true
+<?!("X", 1)>
+<?!("COND:BSTR", 2, "UPLOAD_LENGTH")>
+"success": true,
+"UploadLength": <?BSTR("UPLOAD_LENGTH")>
+<?!("X", 2)>
+}
