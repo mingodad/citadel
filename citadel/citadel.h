a845b4fa9330e996a5cc62fdc5347ac982e855bf
Art Cancro <ajc@uncensored.citadel.org>
2015-04-01 12:58:03 -0400

    Ran the releaseversion.sh script to mark everything as version 9.01

--- citadel/citadel.h
+++ citadel/citadel.h
@@ -1,7 +1,7 @@
 /*
  * Main Citadel header file
  *
- * Copyright (c) 1987-2014 by the citadel.org team
+ * Copyright (c) 1987-2015 by the citadel.org team
  *
  * This program is open source software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 3.
@@ -45,10 +45,10 @@ extern "C" {
  * usually more strict because you're not really supposed to dump/load and
  * upgrade at the same time.
  */
-#define REV_LEVEL	830		/* This version */
+#define REV_LEVEL	901		/* This version */
 #define REV_MIN		591		/* Oldest compatible database */
 #define EXPORT_REV_MIN	760		/* Oldest compatible export files */
-#define LIBCITADEL_MIN	829		/* Minimum required version of libcitadel */
+#define LIBCITADEL_MIN	901		/* Minimum required version of libcitadel */
 
 #define SERVER_TYPE 0			/* zero for stock Citadel; other developers please
 					   obtain SERVER_TYPE codes for your implementations */
--- webcit/webcit.h
+++ webcit/webcit.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1987-2014 by the citadel.org team
+ * Copyright (c) 1987-2015 by the citadel.org team
  *
  * This program is open source software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License version 3.
@@ -127,9 +127,9 @@ extern char *ssl_cipher_list;
 #define PORT_NUM		2000		/* port number to listen on */
 #define DEVELOPER_ID		0
 #define CLIENT_ID		4
-#define CLIENT_VERSION		829		/* This version of WebCit */
-#define MINIMUM_CIT_VERSION	829		/* Minimum required version of Citadel server */
-#define	LIBCITADEL_MIN		821		/* Minimum required version of libcitadel */
+#define CLIENT_VERSION		901		/* This version of WebCit */
+#define MINIMUM_CIT_VERSION	901		/* Minimum required version of Citadel server */
+#define	LIBCITADEL_MIN		901		/* Minimum required version of libcitadel */
 #define DEFAULT_HOST		"localhost"	/* Default Citadel server */
 #define DEFAULT_PORT		"504"
 #define TARGET			"webcit01"	/* Window target for inline URL's */
