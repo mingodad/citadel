bbaf6e447df6dea17b47fbd8c840fbae0a98454f
Art Cancro <ajc@citadel.org>
2016-04-20 10:23:16 -0400

    copyright date

--- webcit/roomops.c
+++ webcit/roomops.c
@@ -1,7 +1,7 @@
 /*
  * Lots of different room-related operations.
  *
- * Copyright (c) 1996-2012 by the citadel.org team
+ * Copyright (c) 1996-2016 by the citadel.org team
  *
  * This program is open source software.  You can redistribute it and/or
  * modify it under the terms of the GNU General Public License, version 3.
